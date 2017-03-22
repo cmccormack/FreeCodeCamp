f4c80fa5ec656821318a8aba065c14538449f307
cmccormack <mccormack.christopher@gmail.com>
2017-03-21 17:32:23 -0700

    Updated refresh button

--- pomodoro/custom.js
+++ pomodoro/custom.js
@@ -1,14 +1,23 @@
+var sessionTime = 25,
+    breakTime = 5;
+
 $('document').ready(function(){
   console.log("Document loaded!")
 
   $('input[type="range"]').on("input change", function(e){
     $target = $(e.target);
-    console.log("Range " + $target.attr("id") + ": " + $target.val());
     changeSlider($(e.target)) 
   });
 
 });
 
 function changeSlider($target){
+  $time = findClosest($target, ".range-group", ".slider-label-time");
+  $time.text($target.val() + " min");
+  console.log($time.text());
 
+}
+
+function findClosest($t, parentSel, targetSel){
+  return $t.closest(parentSel).find(targetSel);
 }
