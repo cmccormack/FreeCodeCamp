c80c67634e8a0ecb83deddfcaff82ed54efb8f95
cmccormack <mccormack.christopher@gmail.com>
2017-03-21 19:31:34 -0700

    Added plus minus click functionality to update slider pos

--- pomodoro/custom.js
+++ pomodoro/custom.js
@@ -1,21 +1,36 @@
 var sessionTime = 25,
-    breakTime = 5;
+    breakTime = 5,
+    step = {
+      "add": 1,
+      "sub": -1
+    };
 
 $('document').ready(function(){
   console.log("Document loaded!")
 
   $('input[type="range"]').on("input change", function(e){
-    $target = $(e.target);
-    changeSlider($(e.target)) 
+    updateLabelTime($(e.target));
   });
+
+  $('.sub, .add').click(function(e){ addSubClick( $(e.target) ); });
+
+
+
 
 });
 
-function changeSlider($target){
-  $time = findClosest($target, ".range-group", ".slider-label-time");
-  $time.text($target.val() + " min");
+function updateLabelTime($t){
+  $time = findClosest($t, ".range-group", ".slider-label-time");
+  $time.text($t.val() + " min");
   console.log($time.text());
+}
 
+function addSubClick($t, direction){
+  var $s = $($t.attr("data-target")),
+      direction = $t.attr("data-op");
+
+  $s.val(Number($s.val()) + step[direction]);
+  updateLabelTime($s);
 }
 
 function findClosest($t, parentSel, targetSel){
