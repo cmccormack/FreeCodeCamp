5a20213f4b021a1b5c6f842ac8f42994a0725ec5
cmccormack <mccormack.christopher@gmail.com>
2017-03-21 22:44:11 -0700

    Replaced stepper labels with buttons and updated js

--- pomodoro/custom.js
+++ pomodoro/custom.js
@@ -1,29 +1,26 @@
 var sessionTime = 25,
-    breakTime = 5,
-    step = {
-      "add": 1,
-      "sub": -1
-    };
+    breakTime = 5;
 
 $('document').ready(function(){
   console.log("Document loaded!")
 
-  $('input[type="range"]').on("input change", function(e){ updateLabelTime($(e.target)); });
-  $('.sub, .add').click(function(e){ addSubClick( $(e.target) ); });
+  $('button').click(function(){ $(this).blur(); });
+  $('input[type="range"]').on("input change", function(e){ updateLabelTime($(this)); });
+  $('.stepper').click(function(){ addSubClick( $(this) ); });
   
 });
 
 function updateLabelTime($t){
   $time = findClosest($t, ".range-group", ".slider-label-time");
-  $time.text($t.val() + " min");
-  console.log($time.text());
+  $time.text($t.val());
+  // console.log($time.text());
 }
 
-function addSubClick($t, direction){
+function addSubClick($t){
   var $s = $($t.attr("data-target")),
-      direction = $t.attr("data-op");
+      inc = $t.attr("data-increment");
 
-  $s.val(Number($s.val()) + step[direction]);
+  $s.val(Number($s.val()) + Number(inc));
   updateLabelTime($s);
 }
 
