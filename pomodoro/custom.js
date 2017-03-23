53938a82ed89a4c3afccc328f465ca624cc723bf
cmccormack <mccormack.christopher@gmail.com>
2017-03-22 22:12:44 -0700

    Countdown function is beginning to now work somewhat

--- pomodoro/custom.js
+++ pomodoro/custom.js
@@ -1,7 +1,10 @@
 var timers = {
-  sessionTime: 25,
-  breakTime: 5
-}
+      sessionTime: 25,
+      breakTime: 5,
+    },
+    intervalID = 0,
+    $clockDisplay,
+    $phase;
 
 $('document').ready(function(){
   console.log("Document loaded!")
@@ -9,19 +12,55 @@ $('document').ready(function(){
   $clockDisplay = $("#display-time");
   $phase = $("clock-phase");
 
-  $('button, input').click(function(){ console.log(JSON.stringify(timers)); }); // For debug purposes
+  // $('button, input').click(function(){ console.log(JSON.stringify(timers)); });
   $('button').click(function(){ $(this).blur(); });
   $('input[type="range"]').on("input change", function(e){ sliderChange($(this)); });
   $('.stepper').click(function(e){ addSubClick( $(this) ); });
-  $('#start-btn').click(function(e){ startCountdown(); });
+  $('#start-btn').click(function(e){ startCountdown(timers.sessionTime, timers.breakTime); });
+  $('#stop-btn').click(function(e){ stopCountdown(); });
 
   
 });
 
-function startCountdown() {
+function startCountdown(sessionTime, breakTime) {
 
+  // Stop the previous countdown first
+  stopCountdown();
+  var startTime = new Date(),
+      countDownTime = startTime.setUTCMinutes(startTime.getUTCMinutes() + sessionTime)
+
+  console.log("sessionTime: " + sessionTime, "breakTime: " + breakTime, 
+    "startTime: " + startTime, "countDownTime: " + countDownTime);
+
+  intervalID = setInterval( function() {
+    
+    var currentTime = new Date().getTime();
+    var delta = countDownTime - currentTime;
+    var minutes = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.ceil((delta % (1000 * 60)) / 1000);
+    if (seconds < 10) { seconds = "0" + seconds }
+    $("#mins").text(minutes);
+    $("#secs").text(seconds);
+    if (seconds % 2 == 0){
+      $("#sep").fadeTo("fast", 0.1);
+      $("#sep").fadeTo("fast", 1);
+    } else {
+      $("#sep").fadeTo("fast", 0.1);
+      $("#sep").fadeTo("fast", 1);
+    }
+  }, 1000);
+  console.log("Timer " + intervalID + " started");
 }
 
+function stopCountdown() {
+  if (intervalID) { 
+    clearInterval(intervalID);
+    console.log("Timer " + intervalID + " stopped"); 
+  }
+  console.log(timers[sessionTime]);
+  displayValue($("#mins"), timers[sessionTime]);
+  displayValue($("#secs"), "00")
+}
 
 function displayValue( $t, val ){
   // console.log($t.attr('id'), val);
