220aca14fcaeb1a713c463da4bbe1510421e9616
cmccormack <mccormack.christopher@gmail.com>
2017-03-22 20:19:41 -0700

    Change function calls to store session and break time in variables

--- pomodoro/custom.js
+++ pomodoro/custom.js
@@ -1,29 +1,54 @@
-var sessionTime = 25,
-    breakTime = 5;
+var timers = {
+  sessionTime: 25,
+  breakTime: 5
+}
 
 $('document').ready(function(){
   console.log("Document loaded!")
 
+  $clockDisplay = $("#display-time");
+  $phase = $("clock-phase");
+
+  $('button, input').click(function(){ console.log(JSON.stringify(timers)); }); // For debug purposes
   $('button').click(function(){ $(this).blur(); });
-  $('input[type="range"]').on("input change", function(e){ updateLabelTime($(this)); });
+  $('input[type="range"]').on("input change", function(e){ sliderChange($(this)); });
   $('.stepper').click(function(e){ addSubClick( $(this) ); });
+  $('#start-btn').click(function(e){ startCountdown(); });
+
   
 });
 
-function updateLabelTime($t){
-  $time = findClosest($t, ".range-group", ".slider-label-time");
-  $time.text($t.val());
-  // console.log($time.text());
+function startCountdown() {
+
 }
 
-function addSubClick($t){
+
+function displayValue( $t, val ){
+  // console.log($t.attr('id'), val);
+  $t.text(val);
+}
+
+
+function sliderChange( $s ){
+  var sliderTime = $s.val(),
+      $label = $( $s.attr("data-display") ),
+      labelID = $label.attr("id");
+
+  timers[labelID] = Number(sliderTime);
+  displayValue($label, timers[labelID]);
+}
+
+
+function addSubClick( $t ){
   var $s = $($t.attr("data-target")),
+      $label = $( $s.attr("data-display") ),
+      labelID = $label.attr("id");
       inc = $t.attr("data-increment");
 
-  $s.val(Number($s.val()) + Number(inc));
-  updateLabelTime($s);
-}
+      // Update slider value after 
+      $s.val(Number($s.val()) + Number(inc));
+      timers[labelID] = Number($s.val());
 
-function findClosest($t, parentSel, targetSel){
-  return $t.closest(parentSel).find(targetSel);
+  // Display new value
+  displayValue($label, timers[labelID]);
 }
