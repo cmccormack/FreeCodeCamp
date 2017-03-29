eb01709bcb3a52c8c271eac02a33868ed0bc6ab7
cmccormack <mccormack.christopher@gmail.com>
2017-03-28 21:19:19 -0700

    Added IDs for cell buttons to be used as positional information

--- tictactoe/custom.js
+++ tictactoe/custom.js
@@ -1,17 +1,40 @@
+var grid = [ ["","",""], ["","",""], ["","",""] ],
+    currentPlayer,
+    $playerSelect;
+
 $('document').ready(function(){
+
+  // Initialize variables
+  $playerSelect = $("#playerSelect label.active input");
+  currentPlayer = $playerSelect.attr('name');
   
-  // Bind functions to events after document has loaded
+  // Bind functions to events
   $('button').click(function(){ $(this).blur(); });
   $('.board-cell').click(function(){ cellClicked($(this)); });
-
-  console.log($(".board-cell"));
+  $('#playerSelect input').on('change', function(){
+    currentPlayer = this.getAttribute("name");
+  });
 
 });
 
 
 
 function cellClicked($cell){
-  var x = $cell.attr("data-row"),
-      y = $cell.attr("data-col");
-      console.log(x,y);
+  var pos = $cell.attr("id").split("_").map(Number),
+      x = pos[0], y = pos[1];
+
+  if (!grid[x][y]){ 
+    grid[x][y] = currentPlayer;
+    displayMove(currentPlayer, x, y);
+  }
+  console.log(currentPlayer, x,y, JSON.stringify(grid));
+}
+
+function displayMove(player, x, y){
+  var $cellIcon = $("#" + x + "_" + y + " i"); 
+  if (player == "X"){
+    $cellIcon.addClass("fa-times");
+  } else {
+    $cellIcon.addClass("fa-circle-o");
+  }
 }
