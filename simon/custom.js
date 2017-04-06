80557688f439486dc0fc874d5e43067245273840
cmccormack <mccormack.christopher@gmail.com>
2017-04-06 09:51:30 -0700

    Added strict mode function

--- simon/custom.js
+++ simon/custom.js
@@ -1,14 +1,19 @@
-
+var isStrict = true;
 
 $('document').ready(function(){
 
   // Bind functions to events after document has loaded
   $('.cell-btn').click(function(){ playSound(this.getAttribute("id")); });
-
+  $('.strict').click(function(){ setStrictMode(); });
 
 });
 
 
+function strictMode(){
+  if (isStrict){
+    isStrict = false;
+  }
+}
 
 function playSound(id){
   $("#" + id + "-audio").get(0).play();
