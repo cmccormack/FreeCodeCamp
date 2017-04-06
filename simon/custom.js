d31adadd415f7afc90ef9aa1080190b8f63170ff
cmccormack <mccormack.christopher@gmail.com>
2017-04-06 10:07:40 -0700

    Modified strict button and added js for text decoration

--- simon/custom.js
+++ simon/custom.js
@@ -3,18 +3,19 @@ var isStrict = true;
 $('document').ready(function(){
 
   // Bind functions to events after document has loaded
-  $('.cell-btn').click(function(){ playSound(this.getAttribute("id")); });
-  $('.strict').click(function(){ setStrictMode(); });
+  $('.cell-btn').click(function(){ playSound(this.getAttribute('id')); });
+  $('#strict').click(function(){ setStrictMode(); });
 
 });
 
 
-function strictMode(){
+function setStrictMode(){
+  $('#strict').toggleClass('strike');
   if (isStrict){
     isStrict = false;
   }
 }
 
 function playSound(id){
-  $("#" + id + "-audio").get(0).play();
+  $('#' + id + '-audio').get(0).play();
 }
