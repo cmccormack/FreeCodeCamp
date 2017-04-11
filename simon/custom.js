21d3fa54c0e3bdbef6afc6736c9b9bca38bd2318
cmccormack <mccormack.christopher@gmail.com>
2017-04-10 20:02:47 -0700

    Code refactored, moved functions around, added separate start/stop functions

--- simon/custom.js
+++ simon/custom.js
@@ -1,41 +1,138 @@
+// Global Variables
 var isStrict = false,
     isPlaying = false,
+    isPlayerTurn = false,
+    audio = true,
     playerIntervalId,
     moveIntervalId,
-    audio = true,
     moves = [],
     playerCnt = 0,
-    maxturns = 2,
-    isPlayerTurn = false,
+    maxturns = 3,
     timeoutSpeed = 5 * 1000,
     moveSpeed = 0.75 * 1000,
     btnArray = ['#red-btn', '#green-btn', '#blue-btn', '#yellow-btn'];
 
+
 $('document').ready(function(){
 
-  resetBoard();
-  // Bind functions to events after document has loaded
-  $('#volume-btn').click(function(){ volumeChange(); });
-  $('.cell-btn').click( (e) => cellClicked( "#" + $(e.currentTarget).attr('id')) );
-  $('#play').click( () => startStop() );
-  $('#strict').click( () => setStrictMode() );
 
+  resetBoard();
+
+  // Bind functions to events after document has loaded
+  $('#volume-btn').click(function(){ toggleAudio(); });
+  $('.cell-btn').click( (e) => cellClicked( "#" + $(e.currentTarget).attr('id')) );
+  $('#play').click( () => togglePlay() );
+  $('#strict').click( () => toggleStrictMode() );
 });
 
+
+var togglePlay = () => {
+  // $('#play i').toggleClass('fa-play fa-stop');
+
+  // Stop game if currently playing
+  if (isPlaying){
+    stopGame();
+  } else {
+    startGame();
+  }
+};
+
+
+var startGame = () => {
+  isPlaying = true;
+  $("#strict").addClass("disabled");
+  displayStatus("");
+  $('#play').attr('title', 'Quit!');
+  $('#play i').addClass('fa-stop');
+  $('#play i').removeClass('fa-play');
+
+  computerTurn();
+
+};
+
+
+var stopGame = () => {
+  $('#play').attr('title', 'Play!');
+  $('#play i').addClass('fa-play');
+  $('#play i').removeClass('fa-stop');
+  
+  resetBoard();
+  console.log('Game stopped, resetting board, isPlaying: ' + isPlaying);
+};
+
+
+var computerTurn = () => {
+
+  moves.push(getRandColor());
+  iterateMoves();
+};
+
+
+var iterateMoves = () => {
+
+  lockBoard();
+  displayCounter(moves.length);
+
+  playerCnt = 0;
+  isPlayerTurn = false;
+
+  var i = 0;
+  moveIntervalId = setInterval( () => {
+    if (i === moves.length) {
+      resetDisplay();
+      clearInterval(moveIntervalId);
+      isPlayerTurn = true;
+      playerTurn();
+    } else {
+      displayMove(moves[i], true);
+    }
+    i++;
+  }, moveSpeed);
+};
+
+
+var displayMove = (move, sound) => {
+  
+  resetDisplay();
+
+  setTimeout( () => {
+    if (sound) { playSound(move + '-audio'); }
+    color = $(move);
+    color.css('opacity', '1');
+  }, 100);
+};
+
+
+var playerTurn = () => {
+
+  unlockBoard();
+
+  // Reset player turn if timeout
+  playerIntervalId = setTimeout( () => {
+    playSound("#buzzer-audio");
+    displayCounter("!!");
+    setTimeout( () => iterateMoves(), 1500);
+  }, timeoutSpeed);
+};
 
 
 var cellClicked = cellid => {
 
-  
   clearInterval(playerIntervalId);
 
-  // Check if wrong cell clicked by player
+  if (!isPlaying) {
+    playSound(cellid + "-audio");
+    displayMove(cellid);
+    return;
+  }
+
+  // Check if wrong cell clicked by player during game
   if (cellid !== moves[playerCnt]){
     playSound('#buzzer-audio');
     if (isStrict){
-      startStop();
+      togglePlay();
       resetBoard();
-      setTimeout( () => { startStop(); }, 1000);
+      setTimeout( () => { togglePlay(); }, 1000);
     } 
     else { setTimeout( () => { iterateMoves(); }, 1000); }
     return;
@@ -43,7 +140,6 @@ var cellClicked = cellid => {
 
   playSound(cellid + "-audio");
 
-  console.log("cellid: " + cellid, "matches moves[playerCnt]: " + moves[playerCnt]);
   playerCnt++;
   if(playerCnt === moves.length){
     clearInterval(playerIntervalId);
@@ -61,15 +157,18 @@ var cellClicked = cellid => {
 
 
 var playerWin = () => {
-  startStop();
+
+  togglePlay();
+  lockBoard();
   displayCounter("=)");
   displayStatus("You Win!");
-  lockBoard();
   playSound('#cheering-audio');
+  $("#play").addClass("disabled");
+  $("#strict").addClass("disabled");
+
   var i = 0,
   winIntervalId = setInterval( () => {
     if (i === 18){
-      
       resetBoard();
       clearInterval(winIntervalId);
     } else {
@@ -80,95 +179,10 @@ var playerWin = () => {
 };
 
 
-var startStop = () => {
-  $('#startStop label i').toggleClass('fa-play fa-stop');
-
-  // Stop game if currently playing
-  if (isPlaying){
-    resetBoard();
-    console.log('Game stopped, resetting board, isPlaying: ' + isPlaying);
-    return;
-  }
-
-  // Moving from Stopped state to Started state
-  isPlaying = true;
-  displayStatus("");
-  $('#play').attr('title', 'Quit!');
-  console.log('Game started, isPlaying: ' + isPlaying);
-
-  computerTurn();
-
-};
 
 
-var computerTurn = () => {
 
-  moves.push(getRandColor());
-  
-  iterateMoves();
-
-};
-
-
-var playerTurn = () => {
-
-  unlockBoard();
-
-  // Reset player turn if timeout
-  playerIntervalId = setTimeout( () => {
-    console.log("Player timed out - id: " + playerIntervalId);
-    playSound("#buzzer-audio");
-    displayCounter("!!");
-    setTimeout( () => iterateMoves(), 1500);
-  }, timeoutSpeed);
-
-  console.log("Player timer started with id: " + playerIntervalId);
-
-};
-
-
-var iterateMoves = () => {
-
-  // Disable user input until moves have been displayed
-  lockBoard();
-
-  displayCounter(moves.length);
-
-  playerCnt = 0;
-  isPlayerTurn = false;
-
-  var i = 0;
-  moveIntervalId = setInterval( () => {
-    console.log("iterateMoves i: " + i, "moves.length: " + moves.length, "i===moves.length: " + (i === moves.length));
-    if (i === moves.length) {
-      resetDisplay();
-      clearInterval(moveIntervalId);
-      isPlayerTurn = true;
-      playerTurn();
-    } else {
-      console.log("iterateMoves calling displayMove(" + moves[i] + ")");
-      displayMove(moves[i], true);
-      
-    }
-    i++;
-
-  }, moveSpeed);
-
-};
-
-
-var displayMove = (move, sound) => {
-  
-  resetDisplay();
-  setTimeout( () => {
-    color = $(move);
-
-    if (sound) { playSound(move + '-audio'); }
-    color.css('opacity', '1');
-  }, 100); // End setTimeout
-};
-
-
+// Utility Functions
 var getRandColor = () => btnArray[Math.floor(Math.random() * btnArray.length)];
 var playSound = id => { if(audio) { $(id).get(0).play(); }};
 var displayCounter = count => $("#counter").val(count<=9?'0'+count:count);
@@ -177,9 +191,10 @@ var resetDisplay = () => { $('.cell-btn', '#display').removeAttr('style'); };
 var lockBoard = () => $('#display').addClass('disabled');
 var unlockBoard = () => $('#display').removeClass('disabled');
 
-
 var resetBoard = () => {
   $('#display').removeClass('disabled');
+  $('#strict').removeClass('disabled');
+  $("#play").removeClass("disabled");
   clearInterval(playerIntervalId);
   clearInterval(moveIntervalId);
   displayCounter("--");
@@ -188,20 +203,17 @@ var resetBoard = () => {
   isPlayerTurn = false;
   playerCnt = 0;
   moves = [];
-  $('#play').attr('title', 'Play!');
+  
   resetDisplay();
+};
 
+var toggleAudio = () => {
+  if (audio) { audio = false; } else { audio = true; }
+  $("#volume-btn i").toggleClass("fa-volume-up fa-volume-off");
 };
 
 
-// Toggles audio
-function volumeChange() {
-  if (audio) { audio = false; } else { audio = true; }
-  $("#volume-btn i").toggleClass("fa-volume-up fa-volume-off");
-}
-
-
-var setStrictMode = () => {
+var toggleStrictMode = () => {
   $('#strict-i').toggleClass('fa-chain-broken fa-chain');
   if (isStrict){
     isStrict = false;
@@ -210,7 +222,6 @@ var setStrictMode = () => {
     isStrict = true;
     $('#strict').attr('title', 'Disable Strict Mode');
   }
-  console.log('Strict mode set to ' + isStrict);
 };
 
 
