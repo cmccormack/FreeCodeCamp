73751e17a76a8fbd9bfd25b1d9e9ec4d8e70f91c
Christopher McCormack <mccormack.christopher@gmail.com>
2017-04-17 20:25:20 -0700

    Moved react code to single file and fixed some bootstrap issues

--- React/markdownviewer/src/index.js
+++ React/markdownviewer/src/index.js
@@ -1,6 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MarkdownViewer from './MarkdownViewer'
+
+
+
+class MarkdownViewer extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      projects: {test: 'testing'}
+    }
+  }
+
+  render() {
+    return (
+ 
+      <div>
+        <AddMarkdownForm />
+      </div>
+    )
+  }
+}
+
+class AddMarkdownForm extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <form>
+          <textarea 
+            cols={50} 
+            rows={20}
+            defaultValue={'this.projects.test'}
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+
+
 
 ReactDOM.render(
   <MarkdownViewer />,
