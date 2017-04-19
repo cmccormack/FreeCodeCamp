0acf814a1b76ce9eb30c2f631e0934d7f27e5578
Christopher McCormack <mccormack.christopher@gmail.com>
2017-04-18 20:32:25 -0700

    fixed issues with reading props from parent

--- React/markdownviewer/src/index.js
+++ React/markdownviewer/src/index.js
@@ -4,40 +4,81 @@ import ReactDOM from 'react-dom'
 
 
 class MarkdownViewer extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
-      projects: {test: 'testing'}
+      inputText: 'This is some text',
+      outputText: ''
     }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  ComponentDidUpdate() {
+    this.setState({inputText: 'testing'})
+  }
+
+  handleChange(event){
+    this.setState({
+      inputText: event.target.value,
+      outputText: event.target.value
+    })
   }
 
   render() {
     return (
  
-      <div>
-        <AddMarkdownForm />
+      <div className="container">
+        <div className="row text-center">
+          <InputForm value={this.state.inputText} onChange={this.handleChange}/>
+          <OutputForm value={this.state.outputText} />
+        </div>
       </div>
     )
   }
 }
 
-class AddMarkdownForm extends React.Component {
+class InputForm extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="col">
         <form>
-          <textarea 
+          <textarea
+            id="inputForm"
+            style={{boxShadow: '0px 0px 12px #444'}}
             cols={50} 
             rows={20}
-            defaultValue={'this.projects.test'}
+            value={this.props.value}
+            onChange={this.props.onChange.bind(this)}
           />
         </form>
       </div>
     )
   }
 }
+
+class OutputForm extends React.Component {
+  render () {
+    return ( 
+      <div className="col">
+        <form>
+          <textarea 
+            id="outputForm"
+            style={{'boxShadow': '0px 0px 12px #444', 'backgroundColor': '#EED'}}
+            cols={50} 
+            rows={20}
+            readOnly
+            value={this.props.value}
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+
 
 
 
