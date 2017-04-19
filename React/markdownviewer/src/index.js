04e1abaa2d5ffdf873cad1eb38b2f48c5679da45
Christopher McCormack <mccormack.christopher@gmail.com>
2017-04-18 22:09:34 -0700

    Added placeholder markdown, project complete to user stories

--- React/markdownviewer/src/index.js
+++ React/markdownviewer/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const marked = require('marked')
+const initText = '# H1\n## H2\n### H3\n#### H4\n##### H5\n###### H6\n\nAlternatively, for H1 and H2, an underline-ish style:\n\nAlt-H1\n======\n\nAlt-H2\n------\nEmphasis, aka italics, with *asterisks* or _underscores_.\n\nStrong emphasis, aka bold, with **asterisks** or __underscores__.\n\nCombined emphasis with **asterisks and _underscores_**.\n\nStrikethrough uses two tildes. ~~Scratch this.~~\n\n1. First ordered list item\n2. Another item\n⋅⋅* Unordered sub-list. \n1. Actual numbers don\'t matter, just that it\'s a number\n⋅⋅1. Ordered sub-list\n4. And another item.\n\n[I\'m an inline-style link](https://www.google.com)\n[I\'m an inline-style link with title](https://www.google.com "Google\'s Homepage")\n\nInline `code` has `back-ticks around` it.\n\nThree or more...\n\n---\n\nHyphens\n\n***\n\nAsterisks\n\n___\n\nUnderscores\n\nTables\n---\n\n| Tables        | Are           | Cool  |\n| ------------- |:-------------:| -----:|\n| col 3 is      | right-aligned | $1600 |\n| col 2 is      | centered      |   $12 |\n| zebra stripes | are neat      |    $1 |\n\n\nImages\n---\n\n![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")'
 
 
 class MarkdownViewer extends React.Component {
@@ -8,21 +10,18 @@ class MarkdownViewer extends React.Component {
     super(props)
 
     this.state = {
-      inputText: 'This is some text',
-      outputText: ''
+      inputText: initText,
+      outputText: marked(initText)
     }
 
     this.handleChange = this.handleChange.bind(this)
   }
 
-  ComponentDidUpdate() {
-    this.setState({inputText: 'testing'})
-  }
 
   handleChange(event){
     this.setState({
       inputText: event.target.value,
-      outputText: event.target.value
+      outputText: marked(event.target.value, {sanitize: true})
     })
   }
 
@@ -30,9 +29,9 @@ class MarkdownViewer extends React.Component {
     return (
  
       <div className="container">
-        <div className="row text-center">
+        <div className="row">
           <InputForm value={this.state.inputText} onChange={this.handleChange}/>
-          <OutputForm value={this.state.outputText} />
+          <OutputDiv value={this.state.outputText} />
         </div>
       </div>
     )
@@ -43,10 +42,10 @@ class InputForm extends React.Component {
 
   render() {
     return (
-      <div className="col">
+      <div className="col text-center">
         <form>
           <textarea
-            id="inputForm"
+            className="inputForm"
             style={{boxShadow: '0px 0px 12px #444'}}
             cols={50} 
             rows={20}
@@ -59,20 +58,16 @@ class InputForm extends React.Component {
   }
 }
 
-class OutputForm extends React.Component {
+class OutputDiv extends React.Component {
   render () {
     return ( 
       <div className="col">
-        <form>
-          <textarea 
-            id="outputForm"
-            style={{'boxShadow': '0px 0px 12px #444', 'backgroundColor': '#EED'}}
-            cols={50} 
-            rows={20}
-            readOnly
-            value={this.props.value}
-          />
-        </form>
+        <div
+          className="outputDiv"
+          style={{'boxShadow': '0px 0px 12px #444', 'backgroundColor': '#EED'}}
+          readOnly
+          ><span dangerouslySetInnerHTML={{__html: this.props.value}} /></div>
+
       </div>
     )
   }
