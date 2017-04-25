c44e17b8e887f2d982288c4ffe04af224f300800
cmccormack <mccormack.christopher@gmail.com>
2017-04-25 11:10:17 -0700

    Cleanup, updated to meet some best practices

--- React/markdownviewer/src/index.js
+++ React/markdownviewer/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const marked = require('marked')
-const initText = '# H1\n## H2\n### H3\n#### H4\n##### H5\n###### H6\n\nAlternatively, for H1 and H2, an underline-ish style:\n\nAlt-H1\n======\n\nAlt-H2\n------\nEmphasis, aka italics, with *asterisks* or _underscores_.\n\nStrong emphasis, aka bold, with **asterisks** or __underscores__.\n\nCombined emphasis with **asterisks and _underscores_**.\n\nStrikethrough uses two tildes. ~~Scratch this.~~\n\n1. First ordered list item\n2. Another item\n⋅⋅* Unordered sub-list. \n1. Actual numbers don\'t matter, just that it\'s a number\n⋅⋅1. Ordered sub-list\n4. And another item.\n\n[I\'m an inline-style link](https://www.google.com)\n[I\'m an inline-style link with title](https://www.google.com "Google\'s Homepage")\n\nInline `code` has `back-ticks around` it.\n\nThree or more...\n\n---\n\nHyphens\n\n***\n\nAsterisks\n\n___\n\nUnderscores\n\nTables\n---\n\n| Tables        | Are           | Cool  |\n| ------------- |:-------------:| -----:|\n| col 3 is      | right-aligned | $1600 |\n| col 2 is      | centered      |   $12 |\n| zebra stripes | are neat      |    $1 |\n\n\nImages\n---\n\n![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")'
+const initText = '# H1\n## H2\n### H3\n#### H4\n##### H5\n###### H6\n\nAlternatively, for H1 and H2, an underline-ish style:\n\nAlt-H1\n======\n\nAlt-H2\n------\nEmphasis, aka italics, with *asterisks* or _underscores_.\n\nStrong emphasis, aka bold, with **asterisks** or __underscores__.\n\nCombined emphasis with **asterisks and _underscores_**.\n\nStrikethrough uses two tildes. ~~Scratch this.~~\n\n1. First ordered list item\n2. Another item\n⋅⋅* Unordered sub-list. \n1. Actual numbers don\'t matter, just that it\'s a number\n⋅⋅1. Ordered sub-list\n4. And another item.\n\n[I\'m an inline-style link](https://www.google.com)\n[I\'m an inline-style link with title](https://www.google.com "Google\'s Homepage")\n\nInline `code` has `back-ticks around` it.\n\nThree or more...\n\n---\n\nHyphens\n\n***\n\nAsterisks\n\n___\n\nUnderscores\n\nTables\n---\n\n| Tables        | Are           | Cool  |\n| ------------- |:-------------:| -----:|\n| col 3 is      | right-aligned | $1600 |\n| col 2 is      | centered      |   $12 |\n| zebra stripes | are neat      |    $1 |\n\n\nImages\n---\n\n![alt text](images/markdown.png "Logo Title Text 1")'
 
 
 class MarkdownViewer extends React.Component {
@@ -18,8 +18,14 @@ class MarkdownViewer extends React.Component {
     this.handleClick = this.handleClick.bind(this)
   }
 
+  shouldComponentUpdate(nextProps, nextState){
+    if (this.props === nextProps && this.state===nextState){
+      return false
+    }
+    return true
+  }
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({
       inputText: event.target.value,
       outputText: marked(event.target.value, {sanitize: true})
@@ -54,9 +60,11 @@ class MarkdownViewer extends React.Component {
  
       <div className="container">
         <div className="row">
-          <InputForm value={this.state.inputText} 
-                     onChange={this.handleChange} 
-                     onClick={this.handleClick} />
+          <InputForm 
+              onChange={this.handleChange} 
+              onClick={this.handleClick}
+              value={this.state.inputText}
+          />
           <OutputDiv value={this.state.outputText} />
         </div>
       </div>
@@ -66,43 +74,73 @@ class MarkdownViewer extends React.Component {
 
 class InputForm extends React.Component {
 
+  shouldComponentUpdate(nextProps, nextState){
+    if (this.props === nextProps && this.state===nextState){
+      return false
+    }
+    return true
+  }
+
   render() {
     return (
       <div className="col-lg-6 col-md-12 text-center">
+
         <form>
           <textarea
-            className="inputForm"
-            style={{boxShadow: '0px 0px 12px #444'}}
-            value={this.props.value}
-            onChange={this.props.onChange.bind(this)}
+              className="inputForm"
+              onChange={this.props.onChange}
+              style={{boxShadow: '0px 0px 12px #444'}}
+              value={this.props.value}
+
+            
           />
         </form>
-        <div id="buttons" className="btn-group">
-        <button type="button" id="clear" className="btn btn-primary" onClick={this.props.onClick.bind(this)}>Clear</button>
-        <button type="button" id="help" className="btn btn-default" onClick={this.props.onClick.bind(this)}>Help</button></div>
+        <div className="btn-group"
+            id="buttons"
+        >
+          <button className="btn btn-primary"
+              id="clear"
+              onClick={this.props.onClick}
+              type="button"
+          >{'Clear'}
+          </button>
+          <button 
+              className="btn btn-default"
+              id="help" 
+              onClick={this.props.onClick} 
+              type="button"
+          >{'Help'}
+          </button>
+        </div>
+
       </div>
     )
   }
 }
 
 class OutputDiv extends React.Component {
+
+  shouldComponentUpdate(nextProps, nextState){
+    if (this.props === nextProps && this.state===nextState){
+      return false
+    }
+    return true
+  }
+
   render () {
     return ( 
       <div className="col">
         <div
-          className="outputDiv"
-          style={{'boxShadow': '0px 0px 12px #444', 'backgroundColor': '#EED'}}
-          readOnly
-          ><span dangerouslySetInnerHTML={{__html: this.props.value}} /></div>
+            className="outputDiv"
+            readOnly
+            style={{'boxShadow': '0px 0px 12px #444', 'backgroundColor': '#EED'}}
+        >
+          <span dangerouslySetInnerHTML={{__html: this.props.value}} /></div>
 
       </div>
     )
   }
 }
-
-
-
-
 
 
 ReactDOM.render(
