d35722d8194e795a23a684eaded262c70767fc95
cmccormack <mccormack.christopher@gmail.com>
2017-04-29 16:50:59 -0700

    Created Leaderboard and LeaderboardHeader components, fixed bugs, page now displays results

--- React/leaderbd/src/index.js
+++ React/leaderbd/src/index.js
@@ -1,21 +1,67 @@
+/*eslint no-console: "off"*/
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+import $ from 'jquery'
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      alltime: [],
+      recent: [],
+      time: 'recent'
     }
   }
 
+  componentDidMount(){
+    this.getApiData(this.props.apiurl, this.state.time)
+  }
+
   shouldComponentUpdate(nextProps, nextState){
-    this.props === nextProps && this.state===nextState ? false : true
+    return this.props === nextProps && this.state===nextState ? false : true
+  }
+
+  getApiData(url, time){
+    $.getJSON(url + time).then( data => {this.setState( {[time]: data })} )
   }
 
   render() {
-    var counter = 0
+    return (
+      <Leaderboard time={this.state[this.state.time]} />
+    )
+  }
+}
 
+class LeaderboardHead extends React.Component {
+
+  shouldComponentUpdate(nextProps, nextState){
+    return this.props === nextProps && this.state===nextState ? false : true
+  }
+
+  render() {
+    return (
+      <thead className="thead-inverse">
+        <tr>
+          <th>{'#'}</th>
+          <th>{'Name'}</th>
+          <th>{'Past 30 Days'}</th>
+          <th>{'All Time'}</th>
+        </tr>
+      </thead>
+    )
+  }
+}
+
+
+class Leaderboard extends React.Component {
+
+  shouldComponentUpdate(nextProps, nextState){
+    return this.props === nextProps && this.state===nextState ? false : true
+  }
+
+  render() {
     return (
  
       <div className="container">
@@ -24,49 +70,50 @@ class App extends React.Component {
         </div>
         <div className="row">
           <table className="table table-sm table-striped table-hover">
-            <thead className="thead-inverse">
-              <tr>
-                <th>{'#'}</th>
-                <th>{'Name'}</th>
-                <th>{'Cookies'}</th>
-              </tr>
-            </thead>
+            <LeaderboardHead />
             <tbody>
-              <User cookies="422" 
-                  name="Chris" 
-                  position={++counter}
-              />
-              <User cookies="100" 
-                  name="David" 
-                  position={++counter}
-              />
+
+              {this.props.time.map( (val,i) =>
+                <User 
+                    cookiesall={val.alltime}
+                    cookiesrecent={val.recent}
+                    img={val.img}
+                    key={val.username}
+                    name={val.username}
+                    position={i+1}
+                />
+              )}
             </tbody>
           </table>
         </div>
       </div>
     )
   }
+
+
 }
 
-
 class User extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-
-    }
-  }
 
   shouldComponentUpdate(nextProps, nextState){
-    this.props === nextProps && this.state===nextState ? false : true
+    return this.props === nextProps && this.state===nextState ? false : true
   }
 
   render() {
     return (
       <tr>
         <th scope="row">{this.props.position}</th>
-        <td>{this.props.name}</td>
-        <td>{this.props.cookies}</td>
+        <td>
+          <a href={'https://www.freecodecamp.com/' + this.props.name}>
+            <img 
+                className="avatar"
+                src={this.props.img} 
+            />
+            {this.props.name}
+          </a>
+        </td>
+        <td>{this.props.cookiesrecent}</td>
+        <td>{this.props.cookiesall}</td>
       </tr>
     )
   }
