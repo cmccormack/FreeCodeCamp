e77ee2af3ba83caed7a21b5ba3df9fdbae845e9c
Christopher McCormack <mccormack.christopher@gmail.com>
2017-05-02 21:11:20 -0700

    Updated Recipe with basic template

--- React/recipes/src/index.js
+++ React/recipes/src/index.js
@@ -46,17 +46,13 @@ class App extends React.Component {
 
   render() {
     return (
-      <Leaderboard 
-          data={this.state[this.state.time]}
-          handleClick={this.handleClick}
-          time={this.state.time}
-      />
+      <RecipeBox />
     )
   }
 }
 
 
-class Leaderboard extends React.Component {
+class RecipeBox extends React.Component {
 
   shouldComponentUpdate(nextProps, nextState){
     return this.props === nextProps && this.state===nextState ? false : true
@@ -67,13 +63,10 @@ class Leaderboard extends React.Component {
  
       <div className="container">
         <div className="row text-center">
-          <span className="title display-3 text-center text-shadow">{'Free Code Camp Leaderboard'}</span>
+          <span className="title display-3 text-center text-shadow">{'Recipe Box'}</span>
         </div>
         <div className="row">
-          <table className="table table-sm table-striped table-hover">
-            <LeaderboardHeader handleClick={this.props.handleClick} />
-            <LeaderboardBody data={this.props.data} />
-          </table>
+          <Recipe />
         </div>
       </div>
     )
@@ -81,86 +74,7 @@ class Leaderboard extends React.Component {
 }
 
 
-class LeaderboardHeader extends React.Component {
-
-  constructor(props) {
-    super(props)
-
-    this._onClick = this._onClick.bind(this)
-  }
-
-  shouldComponentUpdate(nextProps, nextState){
-    return this.props === nextProps && this.state===nextState ? false : true
-  }
-
-  _onClick(event){
-    this.props.handleClick(event, event.currentTarget.id)
-  }
-
-  render() {
-    return (
-      <thead className='thead-inverse unselectable'>
-        <tr>
-          <th width='5%' className='text-center'>{'#'}</th>
-          <th width="40%">{'Name'}</th>
-          <th width="25%">{'Last Updated'}</th>
-          <th 
-              className='header-sort'
-              id='recent'
-              onClick={this._onClick}
-              width='15%'
-          >{'Past 30 Days'}
-            <i 
-                className='fa fa-fw fa-lg fa-unsorted i-sort'
-                id='recent-i'
-            />
-          </th>
-          <th 
-              className='header-sort'
-              id='alltime'
-              onClick={this._onClick}
-              width='15%'
-          >{'All Time'}
-            <i 
-                className='fa fa-fw fa-lg fa-unsorted i-sort'
-                id='alltime-i'
-            />
-          </th>
-        </tr>
-      </thead>
-    )
-  }
-}
-
-
-class LeaderboardBody extends React.Component {
-
-  shouldComponentUpdate(nextProps, nextState){
-    return this.props === nextProps && this.state===nextState ? false : true
-  }
-
-  render() { 
-    return (
-      <tbody>
-
-        {this.props.data.map( (val,i) =>
-          <User 
-              cookiesall={val.alltime}
-              cookiesrecent={val.recent}
-              img={val.img}
-              lastUpdate={val.lastUpdate}
-              key={val.username}
-              name={val.username}
-              position={i+1}
-          />
-        )}
-      </tbody>
-    )
-  }
-}
-
-
-class User extends React.Component {
+class Recipe extends React.Component {
 
   shouldComponentUpdate(nextProps, nextState){
     return this.props === nextProps && this.state===nextState ? false : true
@@ -168,28 +82,18 @@ class User extends React.Component {
 
   render() {
     return (
-      <tr>
-        <th scope="row" className="text-center">{this.props.position}</th>
-        <td>
-          <a 
-            href={'https://www.freecodecamp.com/' + this.props.name}
-            target="_blank"
-          >
-            <img 
-                alt={this.props.name}
-                className="avatar"
-                src={this.props.img}
-            />
-            {this.props.name}
-          </a>
-        </td>
-        <td>{this.props.lastUpdate}</td>
-        <td>{this.props.cookiesrecent}</td>
-        <td>{this.props.cookiesall}</td>
-      </tr>
+ 
+      <div className="col-xs-6">
+        <div className="recipe">
+          {'Recipe'}
+        </div>
+      </div>
     )
   }
 }
+
+
+
 
 
 ReactDOM.render(
