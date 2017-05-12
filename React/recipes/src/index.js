22119bd701b3d5d2570dbf04b6a10541784628f9
cmccormack <mccormack.christopher@gmail.com>
2017-05-11 18:13:36 -0700

    Changed double quotes to single quotes

--- React/recipes/src/index.js
+++ React/recipes/src/index.js
@@ -80,26 +80,6 @@ class App extends React.Component {
 }
 
 
-class NewRecipeButton extends React.Component {
-
-  shouldComponentUpdate(nextProps, nextState){
-    return this.props === nextProps && this.state===nextState ? false : true
-  }
-
-
-
-  render() {
-    return (
-      <div className="col-sm-6 col-md-4">
-        <div className='newRecipeButton'>
-          <div className="inner">{'Add New Recipe'}<br /><i className='fa fa-fw fa-2x fa-plus-square-o' /></div>
-          
-        </div>
-      </div>
-    )
-  }
-}
-
 
 class RecipeBox extends React.Component {
 
@@ -110,11 +90,11 @@ class RecipeBox extends React.Component {
   render() {
     return (
  
-      <div className="container">
-        <div className="row text-center">
-          <span className="title display-3 text-center text-shadow">{'Recipe Box'}</span>
+      <div className='container'>
+        <div className='row text-center'>
+          <span className='title display-3 text-center text-shadow'>{'Recipe Box'}</span>
         </div>
-        <div className="row">
+        <div className='row'>
           {this.props.recipes.map( (recipe) => 
             <Recipe 
                 key={recipe.recipeName} 
@@ -146,29 +126,29 @@ class Recipe extends React.Component {
   render() {
     return (
       
-      <div className="col-sm-6 col-lg-4">
-        <div className="container recipe">
+      <div className='col-sm-6 col-lg-4'>
+        <div className='container recipe'>
 
-          <div className="row recipe-title">
-            <div className="col">
+          <div className='row recipe-title'>
+            <div className='col'>
               {this.r.recipeName}
             </div>
           </div>
 
-          <div className="row">
-            <div className="col prep-time recipe-time">
+          <div className='row'>
+            <div className='col prep-time recipe-time'>
               {'Prep Time: '+ this.r.time.prep + 'm'}
             </div>
-            <div className="col cook-time recipe-time">
+            <div className='col cook-time recipe-time'>
               {'Cook Time: ' + this.r.time.cook + 'm'}
             </div>
           </div>
 
           
-          <div className="row"><div className="col section-title">{'Ingredients'}</div></div>
-          <div className="row">
-            <div className="col">
-              <div className="recipe-ingredients recipe-section">
+          <div className='row'><div className='col section-title'>{'Ingredients'}</div></div>
+          <div className='row'>
+            <div className='col'>
+              <div className='recipe-ingredients recipe-section'>
                 { this.r.ingredients.map( (ingredient) => 
                   <RecipeIngredient 
                       ingredient={ingredient} 
@@ -179,10 +159,10 @@ class Recipe extends React.Component {
             </div>
           </div>
 
-          <div className="row"><div className="col section-title">{'Directions'}</div></div>
-          <div className="row">
-            <div className="col">
-              <div className="recipe-directions recipe-section">
+          <div className='row'><div className='col section-title'>{'Directions'}</div></div>
+          <div className='row'>
+            <div className='col'>
+              <div className='recipe-directions recipe-section'>
                 <ol>
                   {this.r.directions.map( dir => <li key={dir}>{dir}</li>)}
                 </ol>
@@ -190,18 +170,18 @@ class Recipe extends React.Component {
             </div>
           </div>
 
-          <div className="row">
-            <div className="col btn-group"
-                role="group"
+          <div className='row'>
+            <div className='col btn-group'
+                role='group'
             >
-              <button className="btn btn-outline-primary" 
-                  type="button"
+              <button className='btn btn-outline-primary'
+                  type='button'
               >{'View'}</button>
-              <button className="btn btn-outline-primary"
-                  type="button"
+              <button className='btn btn-outline-primary'
+                  type='button'
               >{'Edit'}</button>
-              <button className="btn btn-outline-danger"
-                  type="button"
+              <button className='btn btn-outline-danger'
+                  type='button'
               >{'Delete'}</button>
             </div>
 
@@ -236,7 +216,7 @@ class RecipeIngredient extends React.Component {
 
   render() {
     return (
-      <div className="form-check">
+      <div className='form-check'>
         <label className='form-check-label'>
           <input 
               className='form-check-input' 
@@ -247,6 +227,27 @@ class RecipeIngredient extends React.Component {
           />
           {this.state.ing}
         </label>
+      </div>
+    )
+  }
+}
+
+
+class NewRecipeButton extends React.Component {
+
+  shouldComponentUpdate(nextProps, nextState){
+    return this.props === nextProps && this.state===nextState ? false : true
+  }
+
+  render() {
+    return (
+      <div className='col-sm-6 col-md-4'>
+        <div className='newRecipeButton'>
+          <div className='inner'
+              data-toggle='modal'
+          >{'Add New Recipe'}<br /><i className='fa fa-fw fa-2x fa-plus-square-o' /></div>
+          
+        </div>
       </div>
     )
   }
