ed97bd28b68c7ff1358f7551acb339633e88a275
Christopher McCormack <mccormack.christopher@gmail.com>
2017-05-15 14:58:11 -0700

    Updated many bootstrap elements to Bootstrap-React Components

--- React/recipes/src/index.js
+++ React/recipes/src/index.js
@@ -1,59 +1,72 @@
 /*eslint no-console: "off"*/
 
-import Modal from 'react-bootstrap'
+import { 
+  InputGroup,
+  FormControl,
+  Modal,
+  Button
+} from 'react-bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import $ from 'jquery'
 
-
-var recipes = [
-  {
-    recipeName: 'Duck-shaped Pickles',
+var recipeTemplate = {
+    recipeName: 'Add New Recipe',
     time: {
-      prep: '10',
-      cook: '10'
+      prep: '',
+      cook: ''
     },
-    ingredients: [
-      ['pickles', true],
-      ['salt', true],
-      ['sugar', false],
-      ['cumin', false],
-      ['celery salt', true],
-      ['black peppercorn, crushed', true],
-      ['knife', true]
-    ],
-    directions: [
-      ['Cut pickles into duck-shaped pieces']
-    ]
+    ingredients: [],
+    directions: []
   },
-  {
-    recipeName: 'Smoked Chicken Thighs',
-    time: {
-      prep: '10',
-      cook: '120'
+  recipes = [
+    {
+      recipeName: 'Duck-shaped Pickles',
+      time: {
+        prep: '10',
+        cook: '10'
+      },
+      ingredients: [
+        ['pickles', true],
+        ['salt', true],
+        ['sugar', false],
+        ['cumin', false],
+        ['celery salt', true],
+        ['black peppercorn, crushed', true],
+        ['knife', true]
+      ],
+      directions: [
+        ['Cut pickles into duck-shaped pieces']
+      ]
     },
-    ingredients: [
-      ['6 chicken thighs', true],
-      ['4 tablespoons soy sauce', true],
-      ['1 teaspoon sesame oil', true],
-      ['3 garlic cloves', true],
-      ['3 scallions', true],
-      ['1/2 tablespoon thyme', true],
-      ['1 teaspoon allspice', true],
-      ['1 teaspoon pepper', true],
-      ['1/2 teaspoon cinnamon', true],
-      ['1/2 teaspoon red pepper', true]
-    ],
-    directions: [
-      ['Mix soy sauce with sesame oil and lightly rub into chicken.'],
-      ['In a food processor, combine garlic cloves, scallions, thyme, allspice, pepper, cinnamon, and red pepper until smooth. Rub onto and underneath skin of chicken thighs.'],
-      ['Seal chicken in a plastic bag, place in fridge, and let marinate for at least 4 hours.'],
-      ['Preheat smoker to 225 degrees Fahrenheit and add cherry wood chips.'],
-      ['Remove chicken from fridge 30 minutes prior to cooking. Remove from marinade and place chicken in smoker.'],
-      ['Smoke for 2 hours, rotating throughout cooking. Chicken thighs will be done once the internal temperature of the meat has reached 165 degrees Fahrenheit.']
-    ]
-  }
-]
+    {
+      recipeName: 'Smoked Chicken Thighs',
+      time: {
+        prep: '10',
+        cook: '120'
+      },
+      ingredients: [
+        ['6 chicken thighs', true],
+        ['4 tablespoons soy sauce', true],
+        ['1 teaspoon sesame oil', true],
+        ['3 garlic cloves', true],
+        ['3 scallions', true],
+        ['1/2 tablespoon thyme', true],
+        ['1 teaspoon allspice', true],
+        ['1 teaspoon pepper', true],
+        ['1/2 teaspoon cinnamon', true],
+        ['1/2 teaspoon red pepper', true]
+      ],
+      directions: [
+        ['Mix soy sauce with sesame oil and lightly rub into chicken.'],
+        ['In a food processor, combine garlic cloves, scallions, thyme, allspice, pepper, cinnamon, and red pepper until smooth. Rub onto and underneath skin of chicken thighs.'],
+        ['Seal chicken in a plastic bag, place in fridge, and let marinate for at least 4 hours.'],
+        ['Preheat smoker to 225 degrees Fahrenheit and add cherry wood chips.'],
+        ['Remove chicken from fridge 30 minutes prior to cooking. Remove from marinade and place chicken in smoker.'],
+        ['Smoke for 2 hours, rotating throughout cooking. Chicken thighs will be done once the internal temperature of the meat has reached 165 degrees Fahrenheit.']
+      ]
+    }
+  ]
 
 class App extends React.Component {
   constructor(props) {
@@ -84,8 +97,25 @@ class App extends React.Component {
 
 class RecipeBox extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: false
+    }
+    this.handleClose = this.handleClose.bind(this)
+    this.handleOpen = this.handleOpen.bind(this)
+  }
+
   shouldComponentUpdate(nextProps, nextState){
     return this.props === nextProps && this.state===nextState ? false : true
+  }
+
+  handleClose() {
+    this.setState({ showModal: false })
+  }
+
+  handleOpen() {
+    this.setState({ showModal: true })
   }
 
   render() {
@@ -102,7 +132,11 @@ class RecipeBox extends React.Component {
                 recipe={recipe}
             /> 
           )}
-          <NewRecipeButton />
+          <NewRecipeButton 
+              handleClose={this.handleClose}
+              handleOpen={this.handleOpen}
+              showModal={this.state.showModal}
+          />
         </div>
       </div>
     )
@@ -145,7 +179,6 @@ class Recipe extends React.Component {
             </div>
           </div>
 
-          
           <div className='row'><div className='col section-title'>{'Ingredients'}</div></div>
           <div className='row'>
             <div className='col'>
@@ -239,17 +272,8 @@ class NewRecipeButton extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      recipe: {
-        recipeName: 'Untitled',
-        time: {
-          prep: '',
-          cook: ''
-        },
-        ingredients: [],
-        directions: []
-      }
     }
-
+    this._onClick = this._onClick.bind(this)
     this.r = this.props.recipe
   }
 
@@ -257,28 +281,28 @@ class NewRecipeButton extends React.Component {
     return this.props === nextProps && this.state===nextState ? false : true
   }
 
-  onClick(event){
-    // $('#recipeModal').modal({
-    //   show: true
-    // })
+  _onClick(event){
+    this.setState({showModal: true})
+    console.log(event.currentTarget)
   }
   render() {
     return (
       <div className='col-xs-10 col-xs-offset-2 col-sm-6 col-lg-4'>
         <div className='newRecipeButton'>
           <div className='inner'
-              onClick={this.onClick}
+              onClick={this.props.handleOpen}
           ><p>{'Add New Recipe'}</p><i className='fa fa-fw fa-2x fa-plus-square-o' /></div>
-          <EditRecipeModal recipe={this.state.recipe} />
         </div>
+        <EditRecipeModal 
+            handleClose={this.props.handleClose}
+            recipe={recipeTemplate}
+            save={this.props.handleSave}
+            showModal={this.props.showModal}
+        />
       </div>
     )
   }
 }
-              // data-backdrop='static'
-              // data-target='#recipeModal'
-              // data-toggle='modal'
-
 
 class EditRecipeModal extends React.Component {
 
@@ -296,103 +320,78 @@ class EditRecipeModal extends React.Component {
 
   render() {
     return (
-      <div 
-          className='modal fade' 
-          id='recipeModal'
-          role='dialog'
-          tabIndex='-1' 
+      <Modal 
+          onHide={this.props.handleClose}
+          show={this.props.showModal}
       >
-        <div 
-            className='modal-dialog'
-            role='document'
-        >
-          <div className='modal-content'>
 
-            <div className='modal-header'>
-              <div className='input-group'
-                  style={{width: '90%'}}
-              >
-                <span className='input-group-addon'>{'Recipe Title'}</span>
-                <input
-                    className='form-control'
-                    placeholder={this.r.title}
-                    type='text' 
-                />
-              </div>
-              <button 
-                  className='close'
-                  data-dismiss='modal'
-                  type='button' 
-              >
-                <i className='fa fa-close' />
-              </button>
-            </div>
+        <Modal.Header closeButton>
+          <Modal.Title>{this.r.recipeName}</Modal.Title>
+        </Modal.Header>
 
-            <div className='modal-body'>
+        <Modal.Body>
 
-              <div className='container-fluid'>
-                
-                <div className='row'>
-                  {['Prep Time', 'Cook Time'].map((item,i) => {
-                    return (
-                      <div 
-                          className="col-sm-6 input-group" 
-                          key={item}
-                      >
-                        <span className="input-group-addon">{item}</span>
-                        <input
-                            className='form-control'
-                            defaultValue={this.r.time[item.split(' ')[0].toLowerCase()]}
-                            placeholder={i*10+10}
-                            type='text'
-                        />
-                        <span className="input-group-addon">{'minutes'}</span>
-                      </div>
-                    )
-                  })}
-                </div>
+          <div className='container-fluid'>
+            <InputGroup>
+              <InputGroup.Addon>{'Recipe Name'}</InputGroup.Addon>
+              <FormControl 
+                  placeholder={this.r.title}
+                  type='text'
+              />
+            </InputGroup>
 
-                <div className='row'>
-                  <div className='col input-group'>
-                    <span className='input-group-addon'>{'Ingredients'}</span>
+            <div className='row'>
+              {['Prep Time', 'Cook Time'].map((item,i) => {
+                return (
+                  <div 
+                      className="col-sm-6 input-group" 
+                      key={item}
+                  >
+                    <span className="input-group-addon">{item}</span>
                     <input
                         className='form-control'
-                        defaultValue={this.r.ingredients.map(item => item[0]).join('; ')}
-                        placeholder='salt; black pepper, ground; pickles'
+                        defaultValue={this.r.time[item.split(' ')[0].toLowerCase()]}
+                        placeholder={i*10+10}
                         type='text'
                     />
+                    <span className="input-group-addon">{'minutes'}</span>
                   </div>
-                </div>
+                )
+              })}
+            </div>
 
-                <div className='row'>
-                  <div className='col input-group'>
-                    <span className='input-group-addon'>{'Directions'}</span>
-                    <textarea
-                        className='form-control'
-                        defaultValue={this.r.directions.join('\n')}
-                        rows='8'
-                    />
-                  </div>
-                </div>
-
+            <div className='row'>
+              <div className='col input-group'>
+                <span className='input-group-addon'>{'Ingredients'}</span>
+                <input
+                    className='form-control'
+                    defaultValue={this.r.ingredients.map(item => item[0]).join('; ')}
+                    placeholder='salt; black pepper, ground; pickles'
+                    type='text'
+                />
               </div>
-
             </div>
 
-            <div className='modal-footer'>
-              <button 
-                  className='btn btn-secondary'
-                  data-dismiss='modal'
-                  type='button' 
-              >{'Close'}</button>
-              <button 
-                  className='btn btn-primary'
-                  type='button'
-              >{'Save changes'}</button>
+            <div className='row'>
+              <div className='col input-group'>
+                <span className='input-group-addon'>{'Directions'}</span>
+                <textarea
+                    className='form-control'
+                    defaultValue={this.r.directions.join('\n')}
+                    rows='8'
+                />
+              </div>
             </div>
+
           </div>
-        </div>
-      </div>
+
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button onClick={this.props.handleClose}>{'Close'}</Button>
+          <Button bsStyle='primary'>{'Save changes'}</Button>
+        </Modal.Footer>
+      </Modal>
     )
   }
 }
