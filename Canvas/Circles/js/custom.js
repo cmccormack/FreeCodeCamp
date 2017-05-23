aead14922d9425e8de270c250de6091116bf7d48
cmccormack <mccormack.christopher@gmail.com>
2017-05-23 14:47:45 -0700

    Adding array of Circles and drawing to screen completed

--- Canvas/Circles/js/custom.js
+++ Canvas/Circles/js/custom.js
@@ -1,9 +1,69 @@
-window.onload = () => {
+// arc(x, y, radius, startAngle, endAngle, anticlockwise)
 
-    var canvas = document.getElementById('canvas')
-    var ctx = canvas.getContext('2d')
-    windowResize()
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+function windowResize() {
+    ['Width', 'Height'].map(i=>canvas[i.toLowerCase()]=window['inner'+i])
+    init(100, 6, 60)
+} 
+
+windowResize()
+
+
+
+
+function Circle(x, y, dx, dy, radius, color) {
+    this.x = x
+    this.y = y
+    this.dx = dx
+    this.dy = dy
+    this.radius = radius
+    this.color = color
+
+    this.update = () => {
+
+
+    }
+
+    this.update = () => {
+
+        this.draw()
+    }
+
+    this.draw = () => {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = color
+        ctx.fill()
+    }
 
 }
 
-var windowResize = () => ['Width', 'Height'].map(i=>canvas[i.toLowerCase()]=window['inner'+i])
+
+function init (numCircles, speedRange, radiusRange) {
+
+    var circleArray = [],
+        x,
+        y,
+        dx,
+        dy,
+        radius,
+        color,
+        circle
+
+    for(let i=0; i < numCircles; i++){
+        x = Math.floor(Math.random() * innerWidth)
+        y = Math.floor(Math.random() * innerHeight)
+        dx = Math.floor(Math.random() * speedRange)
+        dy = Math.floor(Math.random() * speedRange)
+        radius = Math.floor(Math.random() * radiusRange)
+        color = 'hsl(' + Math.floor(Math.random() * 256) + ',60%,60%)'
+
+        circle = new Circle(x, y, dx, dy, radius, color)
+        circleArray.push(circle)
+        circle.draw()
+    }
+
+
+}
+
