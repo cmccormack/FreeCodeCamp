2624acc6083dc0dba4e665179671a0b046f69ccf
Christopher McCormack <mccormack.christopher@gmail.com>
2017-05-29 21:35:20 -0700

    Working on cell toggle state

--- React/gameoflife/src/index.js
+++ React/gameoflife/src/index.js
@@ -8,8 +8,8 @@ import ReactDOM from 'react-dom'
 
 var grid = [],
   boardSize = {
-    width: 50,
-    height: 30
+    width: 5,
+    height: 10
   },
   cellSize = {
     width: 16,
@@ -25,7 +25,6 @@ class App extends React.Component {
 
     }
   }
-
 
   shouldComponentUpdate(nextProps, nextState){
     return this.props === nextProps && this.state===nextState ? false : true
@@ -58,9 +57,7 @@ class App extends React.Component {
         </Row>
       </Grid>
     )
-
   }
-
 }
 
 
@@ -68,6 +65,7 @@ class Board extends React.Component {
 
   constructor(props){
     super(props)
+    this.handleCellClick = this.handleCellClick.bind(this)
     this.state = {
       grid: this.initializeGrid()
     }
@@ -77,22 +75,35 @@ class Board extends React.Component {
     return this.props === nextProps && this.state===nextState ? false : true
   }
 
+  handleCellClick(cell){
+    var grid = this.state.grid.slice(0)
+    console.log(grid[cell.dataset.pos])
+    // if (cell.classList.contains('alive')){
+    //   cell.classList.remove('alive')
+    // } else {
+    //   cell.classList.add('alive')
+    // }
+  }
+
   initializeGrid() {
-    for (let row = 0; row < this.props.boardSize.height; row++){
+    let grid = []
+    for (let row = 0, pos; row < this.props.boardSize.height; row++){
       for (let col = 0; col < this.props.boardSize.width; col++){
+        pos = this.props.boardSize.width * row + col
         grid.push(
           <Cell 
               boardSize={this.props.boardSize}
-              col={col}
+              handleClick={this.handleCellClick}
               height={this.props.cellSize.height}
-              key={row + ',' + col}
-              row={row}
-              style={{clear: col % this.props.boardSize.width === 0 ? 'both' : ''}}
+              key={pos}
+              pos={pos}
               width={this.props.cellSize.width}
           />
         )
       }
     }
+    console.log(grid)
+    return grid
   }
 
   render() {
@@ -101,13 +112,13 @@ class Board extends React.Component {
       width: (this.props.boardSize.width * (this.props.cellSize.width-1)) + 'px',
       height: (this.props.boardSize.height * (this.props.cellSize.height-1)) + 'px'
     }
-    console.log(grid)
+
     return (
       <div 
           className='board' 
           style={boardDivStyle}
       >
-        {grid}
+        {this.state.grid}
       </div>
     )
   }
@@ -121,10 +132,13 @@ function Cell(props) {
     height: props.height + 'px'
   }
 
+  var handleClick = (e) => props.handleClick(e.currentTarget)
+
   return (
     <div 
-        className='cell'
-        data-pos={props.row + ',' + props.col}
+        className={props.className || 'cell'}
+        data-pos={props.pos}
+        onClick={handleClick}
         style={cellStyle}
     />
   )
