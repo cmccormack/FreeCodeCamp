266e338807e8702cf2d7f2571752cd3efdf27553
cmccormack <mccormack.christopher@gmail.com>
2017-05-31 11:09:37 -0700

    Board Update now works within the rules of the game.  Moved cell click handler up to Board

--- React/gameoflife/src/index.js
+++ React/gameoflife/src/index.js
@@ -10,7 +10,7 @@ var globals = {
   board: [],
   boardSize: {
     columns: 10,
-    rows: 15,
+    rows: 10,
     cells: 0,
     padding: 10
   },
@@ -26,7 +26,6 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      
     }
   }
 
@@ -35,7 +34,7 @@ class App extends React.Component {
   }
 
   componentDidUpdate(){
-    console.log(this.state)
+
   }
 
   render() {
@@ -54,7 +53,7 @@ class App extends React.Component {
 
         <Row>
           <Col sm={12}>
-            <Board />
+            <Board board={this.props.board} />
           </Col>
         </Row>
 
@@ -68,8 +67,11 @@ class Board extends React.Component {
 
   constructor(props){
     super(props)
+    this.updateBoard = this.updateBoard.bind(this)
+    this.handleCellClick = this.handleCellClick.bind(this)
+    this.handleClearClick = this.handleClearClick.bind(this)
     this.state = {
-      board: globals.board
+      board: this.props.board
     }
   }
 
@@ -77,11 +79,25 @@ class Board extends React.Component {
     return this.props === nextProps && this.state===nextState ? false : true
   }
 
+  handleCellClick(){
+    this.setState({board: globals.board.slice(0)})
+  }
+
+  handleClearClick(){
+    for (let cell in globals.board) globals.board[cell].alive = false
+    this.setState({board: globals.board.slice(0)})
+  }
+
+  updateBoard(){
+    updateBoard()
+    this.setState({board: globals.board.slice(0)})
+    displayBoard(this.state.board)
+  }
 
   render() {
     const boardDivStyle = {
       width: (globals.boardSize.columns * (globals.cellSize.width-1)+(2*globals.boardSize.padding)) + 'px',
-      height: (globals.boardSize.rows * (globals.cellSize.height-1)+(2*globals.boardSize.padding)) + 'px',
+      height: '100%',
       padding: globals.boardSize.padding
     }
 
@@ -90,16 +106,21 @@ class Board extends React.Component {
           className='board' 
           style={boardDivStyle}
       >
+
         {this.state.board.map( (cell) => {
           return (
             <Cell 
-                class={cell.class}
+                alive={cell.alive}
                 handleClick={this.handleCellClick}
                 id={cell.id}
-                key={cell.key}
+                key={cell.id + (cell.alive ? 'alive':'dead')}
             />
           )
         })}
+        <p><a style={{color: 'white'}} onClick={this.updateBoard}>{'Update Board'}</a></p>
+        <p><a style={{color: 'white'}} onClick={()=> displayBoard(this.state.board)}>{'Display Board'}</a></p>
+        <p><a style={{color: 'white'}} onClick={()=> displayBoard(globals.board)}>{'Display Global Board'}</a></p>
+        <p><a style={{color: 'white'}} onClick={this.handleClearClick}>{'Clear Board'}</a></p>
       </div>
     )
   }
@@ -112,7 +133,7 @@ class Cell extends React.Component {
     super(props)
     this.handleClick = this.handleClick.bind(this)
     this.state = {
-      class: this.props.class
+      alive: this.props.alive
     }
   }
 
@@ -122,17 +143,15 @@ class Cell extends React.Component {
 
 
   handleClick(event) {
-    var cellpos = event.currentTarget.dataset.pos,
-      className = this.state.class.includes('dead') ? 'cell alive' : 'cell dead'
-    this.setState({class: className})
-    globals.board[cellpos].class = className
-    getLiveNeighbors(cellpos)
+    globals.board[event.currentTarget.dataset.pos].alive = !this.state.alive
+    this.props.handleClick()
   }
 
   render() {
+
     return (
       <div 
-          className={this.state.class}
+          className={this.state.alive ? 'cell alive' : 'cell dead'}
           data-pos={this.props.id}
           id={this.props.id}
           onClick={this.handleClick}
@@ -155,50 +174,73 @@ window.onload = function(){
 
 var initializeBoard = () => {
 
-  let row, col, up, down,
-    validCol = col => col >= 0 && col < globals.boardSize.columns,
-    validRow = row => row >= 0 && row < globals.boardSize.rows
-
   globals.boardSize.cells = globals.boardSize.columns * globals.boardSize.rows
 
-  function findNeighbors(row, col){
-    let neighbors = []
-    if ( validRow(row - 1) ) {
-      up = i - globals.boardSize.columns
-      neighbors.push(up)
-      if ( validCol(col - 1) ) neighbors.push(up - 1)
-      if ( validCol(col + 1) ) neighbors.push(up + 1)
-    }
-    if ( validRow(row + 1) ) {
-      down = i + globals.boardSize.columns
-      neighbors.push(down)
-      if ( validCol(col - 1) ) neighbors.push(down - 1)
-      if ( validCol(col + 1) ) neighbors.push(down + 1)
-    }
-    if ( validCol(col - 1) ) neighbors.push(i - 1)
-    if ( validCol(col + 1) ) neighbors.push(i + 1)
+  let row, col, up, down,
+    // Functions to find all possible valid neighbors for each cell
+    validCol = col => col >= 0 && col < globals.boardSize.columns,
+    validRow = row => row >= 0 && row < globals.boardSize.rows,
+    findNeighbors = (row, col) => {
+      let neighbors = []
+      if ( validRow(row - 1) ) {
+        up = i - globals.boardSize.columns
+        neighbors.push(up)
+        if ( validCol(col - 1) ) neighbors.push(up - 1)
+        if ( validCol(col + 1) ) neighbors.push(up + 1)
+      }
+      if ( validRow(row + 1) ) {
+        down = i + globals.boardSize.columns
+        neighbors.push(down)
+        if ( validCol(col - 1) ) neighbors.push(down - 1)
+        if ( validCol(col + 1) ) neighbors.push(down + 1)
+      }
+      if ( validCol(col - 1) ) neighbors.push(i - 1)
+      if ( validCol(col + 1) ) neighbors.push(i + 1)
 
-    return neighbors
-  }
+      return neighbors
+    }
+    
   
   for (var i=0; i < globals.boardSize.cells; i++){
-
     row = Math.floor(i/globals.boardSize.columns)
     col = i % globals.boardSize.columns
 
     globals.board.push({
       id: i,
-      key: i,
       neighbors: findNeighbors(row,col),
-      class: Math.random() > 0.7 ? 'cell alive' : 'cell dead'})
+      alive: Math.random() > 0.7 ? true : false})
   }
   return globals.board
 }
 
-var getLiveNeighbors = (cellpos) => {
-  return globals.board[cellpos].neighbors.reduce( (acc, pos) => 
-    acc + (globals.board[pos].class.includes('alive') ? 1 : 0)
-  , 0)
+var getLiveNeighbors = (cellpos) => 
+  globals.board[cellpos].neighbors.reduce( (acc, pos) => 
+    acc + (globals.board[pos].alive ? 1 : 0), 0)
 
 
+var updateBoard = () => {
+
+  var liveNeighbors
+
+  for (let cell in globals.board){
+    liveNeighbors = getLiveNeighbors(cell)
+    // globals.board[cell].nextState = globals.board[cell].alive
+    if (globals.board[cell].alive){
+      globals.board[cell].nextState = ((liveNeighbors === 2 || liveNeighbors === 3) ? true : false)
+    } else {
+      globals.board[cell].nextState = (liveNeighbors === 3 ? true : false)
+    }
+  }
+
+  for (let cell in globals.board) globals.board[cell].alive = globals.board[cell].nextState
+}
+
+var displayBoard = (board) => {
+  let str = ''
+  for (let cell = 0; cell < globals.boardSize.cells; cell++){
+    str += board[cell].alive ? '1' : '0'
+    str += (cell+1) % globals.boardSize.columns === 0 ? '\n' : ' '
+  }
+
+  console.log(str)
 }
