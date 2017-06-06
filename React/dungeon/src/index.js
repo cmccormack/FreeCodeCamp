a6fcecbf9bdc574247a0da1ac943719d1d909e5f
cmccormack <mccormack.christopher@gmail.com>
2017-06-05 20:01:29 -0700

    Started work on map

--- React/dungeon/src/index.js
+++ React/dungeon/src/index.js
@@ -1,14 +1,53 @@
-import {Grid, Row, Col} from 'react-bootstrap'
+// import {Grid, Row, Col} from 'react-bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+var globals = {
+  map: {
+    width: 70,
+    height: 50,
+    cells: 0
+  }
+}
+
+
+window.onload = function(){
+  ReactDOM.render(<App map={globals.map} />, document.getElementById('root'))
+}
+
 class App extends React.Component {
 
-  // These methods are called when an instance of a component is being created and inserted into the DOM:
-  // constructor()
-  // componentWillMount()
-  // render()
-  // componentDidMount()
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  componentWillUpdate(){
+    initializeBoard()
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    return this.props === nextProps && this.state===nextState ? false : true
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div className='title display-2 text-center text-shadow unselectable'>
+          {'Dungeon Roguelike'}
+        </div>
+        <div>
+          <Board map={this.props.map} />
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class Board extends React.Component {
+
   constructor(props){
     super(props)
     this.state = {}
@@ -20,22 +59,22 @@ class App extends React.Component {
 
   render() {
 
+    var cells = []
+    // for (row =0)
+
     return (
       <div>
-        <Grid>
-          <Row>
-            <Col className='title display-3 text-center text-shadow unselectable'>
-              {'Dungeon Roguelike'}
-            </Col>
-          </Row>
-        </Grid>
+        
       </div>
     )
   }
-
-
-
 }
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+var initializeBoard = () => {
+  globals.board.cells = globals.board.width * globals.board.height
+  var board = []
+  for (var cell in globals.board.cells){
+    board.push({id: cell})
+  }
+}
