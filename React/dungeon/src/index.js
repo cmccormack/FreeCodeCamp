6a103c3665d4439180e2f0e6a98a5f745d404ef4
Christopher McCormack <mccormack.christopher@gmail.com>
2017-06-06 22:48:25 -0700

    Added intercept method and working on checking for intercept before adding rooms

--- React/dungeon/src/index.js
+++ React/dungeon/src/index.js
@@ -5,18 +5,23 @@ import ReactDOM from 'react-dom'
 
 var map = {
   tiles: [],
-  cols: 10,
-  rows: 10,
+  cols: 100,
+  rows: 80,
   padding: 12,
   tile:{
-    width: 24,
-    height: 24
+    width: 8,
+    height: 8
+  },
+  rooms: {
+    MAXROOMS:10,
+    MIN: 15,
+    MAX: 30
   }
 }
 
 
 window.onload = function(){
-  ReactDOM.render(<App map={map} />, document.getElementById('root'))
+  ReactDOM.render(<App />, document.getElementById('root'))
 }
 
 class App extends React.Component {
@@ -29,15 +34,16 @@ class App extends React.Component {
   }
 
   componentWillMount(){
-
     map.tiles = initializeTiles(map.rows, map.cols, 
-        {
-          class:'tile wall hidden'
-        }
-      )
+      {
+        class:'tile wall hidden'
+      }
+    )
 
-    var rect = new Rect(0, 0, 2, 2)
-    rect.draw(map.tiles)
+    var rooms = generateRooms()
+    for (var item in rooms){
+      rooms[item].draw(map.tiles)
+    }
     
 
     this.setState({
@@ -100,26 +106,26 @@ class Map extends React.Component {
 }
 
 
-var Tile = (props) => {
+function Tile(props) {
   
   var {x, y} = props.pos
 
   return (
     <div
-      className={props.tile.class}
-      id={(y*map.cols + x)}
-      key={(y+1) * (x+1)}
-      style={{
-        width: map.tile.width,
-        height: map.tile.height,
-        clear: x === map.cols ? 'both' : 'none'
-      }}
+        className={props.tile.class}
+        id={(y*map.cols + x)}
+        key={(y+1) * (x+1)}
+        style={{
+          width: map.tile.width,
+          height: map.tile.height,
+          clear: x === map.cols ? 'both' : 'none'
+        }}
     />
   )
 }
 
 
-var initializeTiles = function(rows, cols, initObj={}) {
+function initializeTiles(rows, cols, initObj={}){
   var tiles = [],
     className = initObj.class || ''
 
@@ -132,30 +138,67 @@ var initializeTiles = function(rows, cols, initObj={}) {
   return tiles
 }
 
+function randRange(m, n){
+  [m,n] = [m,n].sort((a,b)=>a-b)
+  return Math.floor((Math.random() * (n-m)) + m)
+}
 
-var Pos = function(x, y) {
+function generateRooms(){
+  var rooms = [], current_room, w, h, x, y
+  for (let i=0, cap=1000; i < map.rooms.MAXROOMS; i++, cap--){
+    w = randRange(map.rooms.MIN, map.rooms.MAX)
+    h = randRange(map.rooms.MIN, map.rooms.MAX)
+    x = randRange(1, map.cols - w - 2)
+    y = randRange(1, map.rows - h - 1)
+    current_room = new Room(x, y, w, h)
+    if (!hasIntercepts(current_room)){
+      rooms.push(current_room)
+    }
+  }
+
+  function hasIntercepts(newRoom){
+    for (let room in rooms){
+      if (rooms[room].intercept(newRoom)){
+        return true
+      }
+    }
+    return false
+  }
+
+  return rooms
+}
+
+
+function Pos(x, y) {
   this.x = x || 0
   this.y = y || 0
   return this
 }
 
-var Rect = function(x, y, w, h) {
-  this.x = x || 0
-  this.y = y || 0
-  this.w = w || window.innerWidth
-  this.h = h || window.innerHeight
+function Room(x, y, w, h) {
+  this.x1 = x
+  this.x2 = x + w - 1
+  this.y1 = y
+  this.y2 = y + h - 1
+  this.w = w
+  this.h = h
   this.center = new Pos(
     this.x + (this.w / 2),
     this.y + (this.h / 2)
   )
-  this.draw = function(map) {
-    console.log(JSON.stringify(map))
-    for (var row = this.y; row < (this.h + this.y); row++){
-      for (var col = this.x; col < this.w + this.x; col++){
-        map[row][col].class = 'tile floor'
-      }
-    }
-    console.log(JSON.stringify(map))
-  }
+
   return this
+}
+
+Room.prototype.draw = function(map){
+  for (var row = this.y; row < (this.h + this.y); row++){
+    for (var col = this.x; col < this.w + this.x; col++){
+      map[row][col].class = 'tile floor'
+    }
+  }
+}
+
+Room.prototype.intercept = function(other){
+  return this.x1 < other.x2 && this.x2 > other.x1 &&
+  this.y1 < other.y2 && this.y2 > other.y1
 }
