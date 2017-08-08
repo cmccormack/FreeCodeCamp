41f7a2f1379a84bf45f227ec911d997ac32dba47
cmccormack <mccormack.christopher@gmail.com>
2017-08-07 22:16:42 -0700

    Added new background image

--- d3/gdpbarchart/src/index.js
+++ d3/gdpbarchart/src/index.js
@@ -1,31 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios'
+
 
 class App extends React.Component {
   constructor(props){
     super(props)
-    this.state = {}
+    this.state = {
+        data: [],
+        description: ''
+    }
   }
 
   componentDidMount(){
-    // Grab ajax data
+    // Grab data from API and store in state once App mounts
+    axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+        .then((response)=>{
+            this.setState({
+                data: response.data,
+                description: response.description,
+                title: 'GDP Bar Chart'
+            })
+        })
   }
 
   shouldComponentUpdate(nextProps, nextState){
     return this.props === nextProps && this.state===nextState ? false : true
   }
 
-render() {
+  render() {
 
     return (
       <div>
-        <div className='title display-2 text-center text-shadow unselectable'>
-          {'GDP Bar Chart'}
-        </div>
+        <TitleBar title={this.state.title} />
+        <ChartBody data={this.state.data} />
       </div>
     )
   }
+}
 
+function TitleBar(props){
+  return <div className='title display-2 text-center text-shadow unselectable'>{props.title}</div>
+}
+
+function ChartBody(props){
+  var data = props.data
+  console.log(data)
+  return <div />
 }
 
 window.onload = function(){
