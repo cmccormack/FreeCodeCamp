bae96387997077a0dde4e9d119d961e016304191
cmccormack <mccormack.christopher@gmail.com>
2017-08-10 16:50:26 -0700

    Setup react for d3 project

--- d3/gdpbarchart/src/index.js
+++ d3/gdpbarchart/src/index.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/*eslint no-console: "off"*/
+
+import React from 'react'
+import ReactDOM from 'react-dom'
 import axios from 'axios'
 
+
+var globals = {
+  url: 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+}
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-        data: [],
-        description: ''
+      data: [],
+      description: ''
     }
   }
 
   componentDidMount(){
     // Grab data from API and store in state once App mounts
-    axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-        .then((response)=>{
-            this.setState({
-                data: response.data,
-                description: response.description,
-                title: 'GDP Bar Chart'
-            })
+    fetch(globals.url)
+      .then((response)=>response.json()).then(json=>{
+        this.setState({
+          data: json.data,
+          description: json.description,
+          title: 'GDP Bar Chart'
         })
+      })
   }
 
   shouldComponentUpdate(nextProps, nextState){
