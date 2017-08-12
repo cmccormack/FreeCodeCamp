9b6118197872298690038be0b0a994393fec1819
Christopher McCormack <mccormack.christopher@gmail.com>
2017-08-12 11:07:54 -0700

    Addin rect elements to svg

--- d3/gdpbarchart/src/index.js
+++ d3/gdpbarchart/src/index.js
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { scaleTime, scaleLinear } from 'd3-scale'
+import { d3 } from 'd3'
 
 
 var globals = {
@@ -38,7 +40,10 @@ class App extends React.Component {
     return (
       <div>
         <TitleBar title={this.state.title} />
-        <ChartBody data={this.state.data} />
+        <ChartBody 
+            data={this.state.data}
+            desc={this.state.description}
+        />
       </div>
     )
   }
@@ -50,21 +55,52 @@ function TitleBar(props){
 
 function ChartBody(props){
 
-  var chart = {
-    className: 'chart',
-    width: 900,
-    height: 600,
-    style: {
-      backgroundColor: '#EEE',
-      boxShadow: '-8px 8px 32px #444'
-    }
-  }
+  var canvas = {
+      className: 'canvas',
+      width: 900,
+      height: 600,
+      style: {
+        backgroundColor: '#EEE',
+        boxShadow: '-8px 8px 32px #444'
+      }
+    },
+    chart = {
+      marginTop: 40,
+      marginRight: 40,
+      marginBottom: 40,
+      marginLeft: 40,
+    },
+    data = props.data,
+    barWidth = 0,
+    x = scaleTime().domain([new Date(data[0]), new Date(data[1])]),
+    y = scaleLinear().range([chart.height, 0])
+    
+    
+  chart.width = canvas.width - chart.marginLeft - chart.marginRight
+  chart.height = canvas.height - chart.marginTop - chart.marginBottom
+  chart.x = chart.marginLeft
+  chart.y = canvas.height - chart.marginBottom
 
-  var data = props.data
-  
+  y.range([0, chart.height])
+  barWidth = chart.width / data.length
+
+  console.log(x)
+  console.log(y)
+
   return (
-    <svg {...chart} >
-      {null}
+    <svg {...canvas} >
+      {data.map((v,i)=>{
+        console.log(v[1])
+        console.log(y(v[1]))
+        return (
+          <rect 
+              height={`${ y(v[1]) }px`}
+              key={v[0]+v[1]}
+              width={`${barWidth}px`}
+              x={chart.x + (i*barWidth)}
+              y={chart.y - v[1]/20}
+          />)
+      })}
     </svg>
   )
 }
