d0e911a7b9704cff0b3e4d8d76033169073c28d1
Christopher McCormack <mccormack.christopher@gmail.com>
2017-08-16 21:32:27 -0700

    Cleaned up and corrected datapoints and axis, added new colors, added names to plots

--- d3/scatterplot/src/index.js
+++ d3/scatterplot/src/index.js
@@ -2,12 +2,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { scaleTime, scaleLinear, scaleOrdinal } from 'd3-scale'
+import { scaleTime, scaleLinear } from 'd3-scale'
 import { axisBottom, axisLeft, axisRight } from 'd3-axis'
-import { timeMin } from 'd3-time'
+import { timeSecond } from 'd3-time'
 import { select } from 'd3-selection'
-import { format } from 'd3-format'
-import { extent } from 'd3-array'
+import { timeFormat } from 'd3-time-format'
+import { extent, max, min } from 'd3-array'
 
 
 
@@ -78,7 +78,7 @@ class App extends React.Component {
 }
 
 function TitleBar(props){
-  return <div className='title display-2 text-center text-shadow unselectable'>{props.title}</div>
+  return <div className='title display-4 text-center text-shadow unselectable'>{props.title}</div>
 }
 
 function CanvasBody(props){
@@ -109,26 +109,32 @@ function CanvasBody(props){
 function Chart(props) {
   console.log('In Chart Component')
   var chart = {
-    marginTop: 40,
-    marginRight: 50,
-    marginBottom: 80,
-    marginLeft: 50,
-  },
-  data = props.data
+      marginTop: 40,
+      marginRight: 50,
+      marginBottom: 80,
+      marginLeft: 50,
+      xAxis: {
+        marginLeft: 15,
+        marginRight: 150
+      }
+    },
+    data = props.data,
+    maxTime = max(data, (d)=>d.Seconds),
+    minTime = min(data, (d)=>d.Seconds)
 
-  chart.xScale = scaleTime().domain(extent(data, (d)=>new Date(d.Seconds))).nice()
+  chart.xScale = scaleTime().domain([new Date((maxTime+15-minTime)*1000), new Date(0)])
+  chart.xScale.ticks(timeSecond.every(30))
   chart.yScale = scaleLinear().domain(extent(data, (d)=>d.Place))
-  // chart.color = scaleLinear().domain([0, data[data.length-1][1]]).range([190, 230])
 
   chart.width = props.canvas.width - chart.marginLeft - chart.marginRight
   chart.height = props.canvas.height - chart.marginTop - chart.marginBottom
   chart.x = chart.marginLeft
   chart.y = props.canvas.height - chart.marginBottom
 
-  chart.xScale.range([chart.x, chart.width + chart.x])
-  chart.yScale.range([chart.height, 0])
+  chart.xScale.range([chart.xAxis.marginLeft + chart.x, chart.width + chart.x - chart.xAxis.marginRight])
+  chart.yScale.range([chart.marginTop, chart.height+chart.marginTop])
 
-  var xAxis = axisBottom(chart.xScale),
+  var xAxis = axisBottom(chart.xScale).tickFormat(timeFormat('%M:%S')),
     yAxis = axisLeft(chart.yScale),
     yAxisRight = axisRight(chart.yScale)
 
@@ -136,36 +142,50 @@ function Chart(props) {
 
   select('.canvas').append('g')
     .attr('class', 'x axis')
-    .attr('transform', `translate(0, ${chart.y})`)
+    .attr('transform', `translate(0, ${chart.y + 10})`)
     .call(xAxis)
   
   select('.canvas').append('g')
     .attr('class', 'y axis')
-    .attr('transform', `translate(${chart.x}, ${chart.marginTop})`)
+    .attr('transform', `translate(${chart.x}, ${0})`)
     .call(yAxis)
 
   select('.canvas').append('g')
     .attr('class', 'y axis')
-    .attr('transform', `translate(${chart.x + chart.width}, ${chart.marginTop})`)
+    .attr('transform', `translate(${chart.x + chart.width}, ${0})`)
     .call(yAxisRight)
   
   console.log(`chart.x: ${chart.x}, chart.y: ${chart.y}, chart.width: ${chart.width}, chart.height: ${chart.height}`)
 
   return (
     <g>
-      {data.map((v,i)=>{
-        console.log(v)
+      {data.map((v)=>{
         return (
           <Circle
-              cx={chart.xScale(v.Seconds)}
+              className={'circle circle-shadow'}
+              cx={chart.xScale(new Date((v.Seconds - minTime)*1000))}
               cy={chart.yScale(v.Place)}
               datum={v}
               handleMouse={props.handleMouse}
               key={v.Name + v.Year}
-              r={10}
+              r={5}
           />
         )
       })}
+      <text 
+          transform={'translate(-500, 350), rotate(-90)'}
+          x={chart.marginLeft}
+          y={chart.y}
+      >
+        {'Ranking'}
+      </text>
+      <text 
+          transform={'translate(230, 50)'}
+          x={chart.marginLeft}
+          y={chart.y}
+      >
+        {'Minutes Behind Fastest Time'}
+      </text>
     </g>
   )
 }
@@ -176,14 +196,19 @@ class Circle extends React.Component {
   constructor(props){
     super(props)
 
-    this.highlightColor = `hsl(${20}, 50%, 80%)`
-    this.fillColor = `hsl(${20}, 50%, 50%)`
-
-    const {cx, cy, r} = props
-    this.attr = {cx, cy, r}
+    this.color = this.props.datum.Doping ? 20 : 120
+    this.highlightColor = `hsl(${this.color}, 80%, 60%)`
+    this.fillColor = `hsl(${this.color}, 80%, 40%)`
+    const {cx, cy, r, className} = props
+    this.attr = {cx, cy, r, className}
 
     this.state = {
-      fill: this.fillColor
+      fill: this.fillColor,
+      fontSize: 10,
+      tag: {
+        marginLeft: 10,
+        marginTop: 4
+      }
 
     }
     this.handleMouseOver = this.handleMouseOver.bind(this)
@@ -196,10 +221,12 @@ class Circle extends React.Component {
 
   handleMouseOver(e){
     this.props.handleMouse(true, {x:e.pageX, y:e.pageY}, this.props.datum)
+    this.setState({fill: this.highlightColor})
   }
 
   handleMouseOut(e){
     this.props.handleMouse(false, {x:e.pageX, y:e.pageY}, this.props.datum)
+    this.setState({fill: this.fillColor})
   }
 
   buildToolTip(){
@@ -215,6 +242,13 @@ class Circle extends React.Component {
             onMouseOut={this.handleMouseOut}
             onMouseOver={this.handleMouseOver}
         />
+        <text
+            fontSize={this.state.fontSize}
+            x={this.props.cx + this.state.tag.marginLeft}
+            y={this.props.cy + this.state.tag.marginTop}
+        >
+          {this.props.datum.Name}
+        </text>
       </g>
     )
   }
@@ -227,7 +261,7 @@ function Tooltip(props){
     <div 
         className='tt'
         style={{
-          left: props.pos.x-500,
+          left: props.pos.x-510,
           top: props.pos.y-150,
           display: props.display ? 'block' : 'none'
         }}
