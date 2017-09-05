bc9ce95cf647140b68ce3c8c8e97ab5d12fbd8ec
Christopher McCormack <mccormack.christopher@gmail.com>
2017-09-04 20:24:45 -0700

    Added social media icons

--- d3/forcedirected/src/components/Layout/Footer.js
+++ d3/forcedirected/src/components/Layout/Footer.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import 'font-awesome/css/font-awesome.css'
-import { 
-  Navbar,
-  Nav,
-  MenuItem,
-  NavItem,
-  NavDropdown
-} from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
+import NavItem from '../NavItem'
 
 function Footer (props) {
 
-  let {url, icon, brand} = props
+  let {url, icon, brand, social} = props
+  let iconStyle = {
+    fontSize: '1.2em'
+  }
+
   return(
     <Navbar 
         collapseOnSelect
@@ -19,38 +18,49 @@ function Footer (props) {
     >
       <Navbar.Header>
         <Navbar.Brand>
-          <a href={url ? url : '#'}>
-            { icon && 
-              <i
-                  className={icon}
-                  style={{marginRight: '5px', lineHeight: icon.height}}
-              />
-            }
-            { brand &&
-              <span>{brand}</span>
-            }
+          <a 
+              href={url}
+              style={{fontSize: '1em'}}
+          >
+            <i
+                className={icon.class}
+                style={{
+                  marginRight: '5px',
+                  lineHeight: icon.height
+                }}
+            />
+            <span >{brand}</span>
           </a>
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
       <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="#">{'Link'}</NavItem>
-          <NavItem eventKey={2} href="#">{'Link'}</NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>{'Action'}</MenuItem>
-            <MenuItem eventKey={3.2}>{'Another action'}</MenuItem>
-            <MenuItem eventKey={3.3}>{'Soemthing else here'}</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>{'Seperated Link'}</MenuItem>
-          </NavDropdown>
-        </Nav>
         <Nav pullRight>
-          <NavItem eventKey={1} href="#">{'Link Right'}</NavItem>
-          <NavItem eventKey={2} href="#">{'Link Right'}</NavItem>
+          {social.map((item, i)=>{
+            return (
+              <NavItem 
+                  eventKey={i}
+                  href={item.url}
+                  key={item.name}
+              >
+                <div 
+                    style={{display: 'inline-block', width: '30px'}}
+                >
+                  <i 
+                      className={item.icon} 
+                      style={iconStyle}
+                  />
+                </div>
+                <span className={'show-on-collapse'}>{item.name}</span>
+              </NavItem>
+            )
+          })}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
 }
 export default Footer
+
+
+
