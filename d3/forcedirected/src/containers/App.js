fb8b9832c379480674fc75297542f2cb8608e028
Christopher McCormack <mccormack.christopher@gmail.com>
2017-09-04 20:26:44 -0700

    Added Footer social media object some minor style changes

--- d3/forcedirected/src/containers/App.js
+++ d3/forcedirected/src/containers/App.js
@@ -52,20 +52,31 @@ class App extends React.Component {
 
     const options = {
       header: {
-        brand: 'Chris McCormack',
-        icon: {icon:'fa fa-lg fa-fw fa fa-bar-chart', height: '0.7'},
+        brand: 'D3 Force Directed Graph',
+        icon: {class:'fa fa-lg fa-fw fa fa-bar-chart', height: '0.7'},
         url:'https://mackville.net'
       },
       title: {
         align: 'center',
-        size: 5,
+        size: 3,
         shadow: '-3px 3px 8px #444'
       },
       canvas: {
         data: this.state.data,
         desc: this.state.description,
         height: 600,
-        width: 900
+        width: 1024
+      },
+      footer: {
+        brand: 'Copyright © 2017 Christopher McCormack',
+        icon: {class: 'fa fa-lg fa-fw fa fa-beer', height: '0.7'},
+        social: [
+          {name: 'Facebook', icon: 'fa fa-facebook', url: 'https://www.facebook.com/christopher.j.mccormack'},
+          {name: 'Twitter', icon: 'fa fa-twitter', url: 'https://twitter.com/chrisjmccormack'},
+          {name: 'Github', icon: 'fa fa-github', url: 'https://github.com/cmccormack'},
+          {name: 'Linkedin', icon: 'fa fa-linkedin', url: 'https://www.linkedin.com/in/christopherjmccormack'}
+        ],
+        url: 'https://mackville.net'
       }
     }
 
@@ -74,8 +85,7 @@ class App extends React.Component {
         <Header {...options.header} />
         <Title {...options.title}>{this.state.title}</Title>
         <CanvasBody {...options.canvas} />
-        <div style={{width: '100px', height: '800px', backgroundColor: 'yellow'}} />
-        <Footer />
+        <Footer {...options.footer} />
       </div>
     )
   }
@@ -84,3 +94,21 @@ class App extends React.Component {
 
 
 export default App
+
+
+{/* <footer class="bg-inverse navbar-inverse unselectable">
+  <a class="navbar-brand white-shadow" href="https://www.facebook.com/christopher.j.mccormack"
+    target="_blank">
+    <i class="fa fa-facebook"></i>
+  </a>
+  <a class="navbar-brand white-shadow" href="https://twitter.com/chrisjmccormack" target="_blank">
+    <i class="fa fa-twitter"></i>
+  </a>
+  <a class="navbar-brand white-shadow" href="https://github.com/cmccormack" target="_blank">
+    <i class="fa fa-github"></i>
+  </a>
+  <a class="navbar-brand white-shadow" href="https://www.linkedin.com/in/christopherjmccormack"
+    target="_blank">
+    <i class="fa fa-linkedin"></i>
+  </a>
+</footer> */}
