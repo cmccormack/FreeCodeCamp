1adb0618134633198bab034248768c10e30c3f0d
cmccormack <mccormack.christopher@gmail.com>
2017-09-05 19:45:41 -0700

    Added d3 Force Directed Graph

--- d3/forcedirected/src/containers/App.js
+++ d3/forcedirected/src/containers/App.js
@@ -1,23 +1,67 @@
-/*eslint no-console: "off"*/
+/*eslint no-console: 'off'*/
 
 import React from 'react'
 
 import { Header, Footer, Title } from '../components/Layout'
 import CanvasBody from '../components/CanvasBody'
+import { select, selectAll, event } from 'd3-selection'
+import { 
+  forceSimulation,
+  forceLink,
+  forceManyBody,
+  forceCenter,
+  forceCollide,
+  forceX,
+  forceY
+} from 'd3-force'
+import { drag } from 'd3-drag'
+import { scaleOrdinal, schemeCategory20 } from 'd3-scale'
 
 
+const nodes = [
+  { id: 'mammal', group: 0, label: 'Mammals', level: 1 },
+  { id: 'dog'   , group: 0, label: 'Dogs'   , level: 2 },
+  { id: 'cat'   , group: 0, label: 'Cats'   , level: 2 },
+  { id: 'fox'   , group: 0, label: 'Foxes'  , level: 2 },
+  { id: 'elk'   , group: 0, label: 'Elk'    , level: 2 },
+  { id: 'insect', group: 1, label: 'Insects', level: 1 },
+  { id: 'ant'   , group: 1, label: 'Ants'   , level: 2 },
+  { id: 'bee'   , group: 1, label: 'Bees'   , level: 2 },
+  { id: 'fish'  , group: 2, label: 'Fish'   , level: 1 },
+  { id: 'carp'  , group: 2, label: 'Carp'   , level: 2 },
+  { id: 'pike'  , group: 2, label: 'Pikes'  , level: 2 }
+]
 
+const links = [
+	{ target: 'mammal', source: 'dog' , strength: 0.7 },
+	{ target: 'mammal', source: 'cat' , strength: 0.7 },
+  { target: 'mammal', source: 'fox' , strength: 0.7 },
+  { target: 'mammal', source: 'elk' , strength: 0.7 },
+  { target: 'insect', source: 'ant' , strength: 0.7 },
+  { target: 'insect', source: 'bee' , strength: 0.7 },
+  { target: 'fish'  , source: 'carp', strength: 0.7 },
+  { target: 'fish'  , source: 'pike', strength: 0.7 },
+  { target: 'cat'   , source: 'elk' , strength: 0.1 },
+  { target: 'carp'  , source: 'ant' , strength: 0.1 },
+  { target: 'elk'   , source: 'bee' , strength: 0.1 },
+  { target: 'dog'   , source: 'cat' , strength: 0.1 },
+  { target: 'fox'   , source: 'ant' , strength: 0.1 },
+	{ target: 'pike'  , source: 'cat' , strength: 0.1 }
+]
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      description: ''
+      canvas: {
+        width: 1024,
+        height: 800
+      }
     }
   }
 
   componentWillMount(){
-    console.log('In componentDidMount')
+    console.log('In componentWillMount')
 
     // Grab data from API and store in state once App mounts
     fetch(this.props.url)
@@ -31,12 +75,17 @@ class App extends React.Component {
           data: json,
           description: 'Bouncy Flags!!!!',
           title: 'National Contiguity using D3 Force Directed Graph'
-        }, ()=>{console.log(this.state.data)}) 
+        })
+        return json
+      })
+      .then((json)=>{
+        buildForceGraph(json.nodes, json.links, this.state.canvas)
       })
   }
 
   componentDidMount(){
-        // Add a span element for the tooltip div to attach within the root element
+    console.log('In componentDidMount')
+    // Add a span element for the tooltip div to attach within the root element
     var tooltip = document.createElement('span')
     tooltip.id='tooltip'
     document.getElementById('root').appendChild(tooltip)
@@ -64,8 +113,8 @@ class App extends React.Component {
       canvas: {
         data: this.state.data,
         desc: this.state.description,
-        height: 600,
-        width: 1024
+        width: this.state.canvas.width,
+        height: this.state.canvas.height
       },
       footer: {
         brand: 'Copyright © 2017 Christopher McCormack',
@@ -76,7 +125,7 @@ class App extends React.Component {
           {name: 'Github', icon: 'fa fa-github', url: 'https://github.com/cmccormack'},
           {name: 'Linkedin', icon: 'fa fa-linkedin', url: 'https://www.linkedin.com/in/christopherjmccormack'}
         ],
-        url: 'https://mackville.net'
+        url: 'https://mackville.net'  
       }
     }
 
@@ -92,23 +141,86 @@ class App extends React.Component {
 }
 
 
+function buildForceGraph(nodes, links, canvas){
+  console.log('in buildForceGraph function')
+
+  const {width, height} = canvas
+
+  const svg = select('svg')
+    .style('background-color', `hsl(${Math.floor(Math.random()*360)}, 20%, 95%)`)
+
+  
+  const simulation = forceSimulation()
+    .force('link', forceLink().id((d)=>d.index))
+    .force('collide', forceCollide( (d)=>15 ).iterations(16) )
+    .force('charge', forceManyBody().strength(-280))
+    .force('center', forceCenter(width / 2, height / 2))
+    .force('y', forceY(0).strength(0.6))
+    .force('x', forceX(0).strength(0.6))
+
+  const link = svg.append('g')
+    .attr('class', 'links')
+    .selectAll('line')
+    .data(links)
+      .enter().append('line')
+        .attr('stroke-width', ()=>2)
+        .style('stroke', '#999')
+        .style('opacity', '0.8')
+
+  // selectAll('.line')
+  //   .style({stroke: '#999', opacity: '0.8'})  
+
+  const node = svg.append('g')
+    .attr('class', 'nodes')
+    .selectAll('circle')
+    .data(nodes)
+      .enter().append('circle')
+        .attr('r', 10)
+        .attr('fill', ()=>`hsl(${Math.floor(Math.random()*360)}, 50%, 50%)`)
+        .call(drag()
+          .on('start', dragstarted)
+          .on('drag', dragged)
+          .on('end', dragended)
+      )
+
+  node.append('title')
+      .text(d=>d.country)
+
+  simulation
+      .nodes(nodes)
+      .on('tick', ticked)
+
+  simulation.force('link')
+      .links(links)
+
+  function ticked() {
+    link
+        .attr('x1', d=>d.source.x)
+        .attr('y2', d=>d.target.y)
+        .attr('y1', d=>d.source.y)
+        .attr('x2', d=>d.target.x)
+
+    node
+        .attr('cx', d=>d.x)
+        .attr('cy', d=>d.y)
+  }
+
+  function dragstarted(d) {
+    if (!event.active) simulation.alphaTarget(0.2).restart()
+    d.fx = d.x
+    d.fy = d.y
+  }
+  
+  function dragged(d) {
+    d.fx = event.x
+    d.fy = event.y
+  }
+  
+  function dragended(d) {
+    if (!event.active) simulation.alphaTarget(0)
+    d.fx = null
+    d.fy = null
+  }
+}
 
 export default App
-
-
-{/* <footer class="bg-inverse navbar-inverse unselectable">
-  <a class="navbar-brand white-shadow" href="https://www.facebook.com/christopher.j.mccormack"
-    target="_blank">
-    <i class="fa fa-facebook"></i>
-  </a>
-  <a class="navbar-brand white-shadow" href="https://twitter.com/chrisjmccormack" target="_blank">
-    <i class="fa fa-twitter"></i>
-  </a>
-  <a class="navbar-brand white-shadow" href="https://github.com/cmccormack" target="_blank">
-    <i class="fa fa-github"></i>
-  </a>
-  <a class="navbar-brand white-shadow" href="https://www.linkedin.com/in/christopherjmccormack"
-    target="_blank">
-    <i class="fa fa-linkedin"></i>
-  </a>
-</footer> */}
