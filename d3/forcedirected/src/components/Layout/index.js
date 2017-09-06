71457211bb9d79fbccf422a80859ae1a9784d440
cmccormack <mccormack.christopher@gmail.com>
2017-09-06 10:25:53 -0700

    Added Tooltip Component

--- d3/forcedirected/src/components/Layout/index.js
+++ d3/forcedirected/src/components/Layout/index.js
@@ -1,5 +1,11 @@
 import Header from './Header'
 import Footer from './Footer'
 import Title  from './Title'
+import Tooltip  from './Tooltip'
 
-export {Header, Footer, Title}
+export {
+    Header,
+    Footer,
+    Title,
+    Tooltip
+}
