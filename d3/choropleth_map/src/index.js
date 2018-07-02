c792e7c651e0c2758fa698b9bdcd60b346e95cc0
Christopher McCormack <mccormack.christopher@gmail.com>
2018-07-01 19:04:44 -0700

    Completed Header component

--- d3/choropleth_map/src/index.js
+++ d3/choropleth_map/src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import PropTypes from "prop-types"
+import cx from "classnames"
 
 import "./assets/styles/styles.scss"
 
@@ -8,54 +9,58 @@ import "./assets/styles/styles.scss"
 class NavList extends React.PureComponent {
 
   static propTypes = {
-    children: PropTypes.array,
+    links: PropTypes.array,
+    children: PropTypes.any,
+    className: PropTypes.string,
   }
 
   static defaultProps = {
-    children: [],
+    links: [],
+    className: "",
   }
 
   render() {
-    const { children } = this.props
-    return (
-      <ul className="navlist">
-        {
-          children.map(item => (
-            <NavItem
-              key={item.name}
-              link={item.link}
-              name={item.name}
-              onClick={item.onClick}
-            />
-          ))
-        }
-      </ul>
-    )
+    const { children, className, links } = this.props
+    return <ul className={cx(className, "navlist")}>
+      {links.map(item => (
+        <NavItem
+          key={item.name}
+          link={item.link}
+          name={item.name}
+          onClick={item.onClick}
+        />
+      ))}
+      {children}
+    </ul>
   }
 }
 
 class NavItem extends React.PureComponent {
+
   static propTypes = {
+    children: PropTypes.any,
+    className: PropTypes.string,
     link: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
+    name: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object,
+    ]),
     onClick: PropTypes.func,
   }
 
   static defaultProps = {
+    className: "",
     onClick: undefined,
   }
 
   render() {
-    const { link, name, onClick } = this.props
+    const { children, className, link, name, onClick } = this.props
     return (
-      <li className="navitem">
-        <a
-          className="navlink"
-          href={link}
-          onClick={onClick}
-        >
+      <li className={cx(className, "navitem")}>
+        <a className="navlink" href={link} onClick={onClick}>
           {name}
         </a>
+        {children}
       </li>
     )
   }
@@ -63,6 +68,10 @@ class NavItem extends React.PureComponent {
 
 
 class Header extends React.Component {
+
+  state = {
+    isVisible: false,
+  }
 
   static propTypes = {
     badge: PropTypes.string,
@@ -72,11 +81,55 @@ class Header extends React.Component {
     badge: "Title",
   }
 
+  componentDidMount() {
+    document.body.addEventListener("click", () => {
+      this.setState({isVisible: false})
+    })
+  }
+
+  handleDropdownClick = () => {
+    this.setState(prevState => ({isVisible: !prevState.isVisible}))
+  }
+
   render() {
 
+    const { isVisible } = this.state
     const links = [
       { name: "Portfolio", link: "https://mackville.net/" },
     ]
+
+    const dropdownbutton = {
+      name: (
+        <span>
+          {"Other D3 Projects "}
+          <span className="caret" />
+        </span>
+      ),
+      link: "#", onClick: this.handleDropdownClick }
+
+    const dropdownitems = [
+      {
+        name: "Force Directed Graph",
+        link: "https://mackville.net/d3/forcedirected/",
+      },
+      {
+        name: "Heatmap",
+        link: "https://mackville.net/d3/heatmap/",
+      },
+      {
+        name: "GDP Bar Chart",
+        link: "https://mackville.net/d3/gdpbarchart/",
+      },
+      {
+        name: "Meteor Strikes",
+        link: "https://mackville.net/d3/meteorstrikes/",
+      },
+      {
+        name: "Scatterplot",
+        link: "https://mackville.net/d3/scatterplot/",
+      },
+    ]
+
     return (
       <header className="z-depth-2">
         <nav id="header-nav">
@@ -84,8 +137,17 @@ class Header extends React.Component {
             {"D3 Choropleth Map"}
           </div>
           <div id="header-links">
-            <NavList>
-              {links}
+            <NavList links={links}>
+              <NavItem {...dropdownbutton} className="dropdown">
+                {
+                  isVisible && (
+                    <NavList
+                      className="z-depth-4"
+                      links={ dropdownitems }
+                    />
+                  )
+                }
+              </NavItem>
             </NavList>
           </div>
         </nav>
@@ -98,7 +160,12 @@ const Footer = () => (
   <footer>
     <span id="created-by">Created By&nbsp;</span>
     <span>
-      <a className="navlink" href="https://mackville.net">Christopher McCormack</a>
+      <a
+        className="navlink"
+        href="https://mackville.net"
+      >
+        {"Christopher McCormack"}
+      </a>
     </span>
   </footer>
 )
