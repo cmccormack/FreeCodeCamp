d55e72bd92d356d871f2d8e09fe54717356cac3f
Christopher McCormack <mccormack.christopher@gmail.com>
2018-10-23 11:49:47 -0700

    Added attribution

--- JavaScript_Algorithms_and_Data_Structures/Basic_Algorithm_Scripting/Find_the_Longest_Word_in_a_String.js
+++ JavaScript_Algorithms_and_Data_Structures/Basic_Algorithm_Scripting/Find_the_Longest_Word_in_a_String.js
@@ -1,18 +1,18 @@
 
-// map and sort
+// @cmccormack map and sort
 const findLongestWordLength=s=>s.split(' ').map(v=>v.length).sort((a,b)=>b-a)[0]
 
-// or with recursion
+// @cmccormack with recursion
 const findLongestWordLength2 = s => {
   const nextSpace = s.indexOf(' ')
   return s.indexOf(' ') === -1 ? s.length : Math.max(s.slice(0, nextSpace).length, findLongestWordLength(s.slice(nextSpace+1)))
 }
 
-// or stupidly ugly one liner with recursion
+// @cmccormack stupidly ugly one liner with recursion
 const findLongestWordLength3=s=>s.indexOf(' ') === -1 ? s.length : Math.max(s.slice(0, s.indexOf(' ')).length, findLongestWordLength(s.slice(s.indexOf(' ')+1)))
 
 
-
+// @alpox for loop
 function findLongestWordLength4(str) {
   let max = 0, counter;
   for(let i = 0; i < str.length; i++) {
@@ -26,10 +26,11 @@ function findLongestWordLength4(str) {
 }
 
 
+// @alpox recursion and tail call optimization
 const findLongestWordLength5=f=([x,...y],t=0,c=0)=>x&&(x==' '?f(y,c>t?c:t,0):f(y,t,c+1))||t
 
 
-// @ezioda004 thinkgy
+// @ezioda004 thingy
 const findLongestWordLength6 = s => {
   return (function helper(arr, l = 0){
     if (arr.length == 0) return l;
