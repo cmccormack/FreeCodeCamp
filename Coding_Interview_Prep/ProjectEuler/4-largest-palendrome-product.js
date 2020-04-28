3d0a230d03b24c02a530c2b98fb765c4d5bca5d4
cmccormack <mack.developer@gmail.com>
2020-04-27 17:14:48 -0700

    continue work on project euler largest palendrome product

---
+++ Coding_Interview_Prep/ProjectEuler/4-largest-palendrome-product.js
@@ -0,0 +1,79 @@
+/*
+
+Project Euler: Problem 4: Largest palindrome product
+
+A palindromic number reads the same both ways. The largest
+palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two n-digit numbers.
+
+9*9 = 81
+9*8 = 72
+9*7 = 63
+8*8 = 64
+8*7 = 56
+8*6 = 48
+7*7 = 49
+7*6 = 42
+
+
+*/
+
+function checkPalindrome(n) {
+  const strN = String(n)
+  const lenN = strN.length
+  const isEven = lenN % 2 === 0;
+  const mid = Math.trunc(lenN/2)
+  let left;
+  let right;
+  if (isEven) {
+    left = strN.slice(0, mid)
+    right = strN.slice(mid, lenN)
+  } else {
+    left = strN.slice(0, mid)
+    right = strN.slice(mid+1, lenN)
+  }
+  return left === right.split().reverse().join('')
+}
+
+function largestPalindromeProduct(n) {
+  // Good luck!
+  const maxOperand = Math.pow(10,n) -1
+  const maxProduct = maxOperand * maxOperand
+  console.log(maxProduct)
+  let max = -Infinity
+  for (let i = maxOperand; i > 0; i-=1) {
+    for (let j = maxOperand; j > 0; j -=1) {
+      if (checkPalindrome(i*j)){
+        console.log(i*j)
+        max = Math.max(max, i*j)
+      }
+      // console.log(i*j)
+    }
+  }
+  return max;
+
+}
+
+const br = `\n---------------------------------------------\n`;
+var timeit = function (label, f, ...args) {
+  console.time(label);
+  result = f(...args);
+  console.log(`${br}RESULT RETURNED: ${result}`);
+  console.timeEnd(label);
+};
+
+const in1 = [
+  ["1", "0", "1", "0", "0"],
+  ["1", "0", "1", "1", "1"],
+  ["1", "1", "1", "1", "1"],
+  ["1", "0", "0", "1", "0"],
+];
+const in2 = [
+  ["1", "0", "1", "0", "0"],
+  ["1", "0", "1", "1", "1"],
+  ["1", "1", "1", "1", "1"],
+  ["1", "0", "1", "1", "1"],
+];
+timeit(`RESULT EXPECTED: 9009\t\tTime`, largestPalindromeProduct, 2);
+// timeit(`RESULT EXPECTED: 906609\t\tTime`, largestPalindromeProduct, 3);
